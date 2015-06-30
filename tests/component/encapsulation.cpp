a98ccbca464a563328696313fb4608773c507008
Hugh Sorby <h.sorby@auckland.ac.nz>
2015-07-01 08:42:41 +1200

    Modify encapsulation tests to how I think they should show the behaviour of the library.

--- tests/component/encapsulation.cpp
+++ tests/component/encapsulation.cpp
@@ -2,7 +2,7 @@
 
 #include <libcellml>
 
-
+#include <iostream>
 TEST(Encapsulation, serialise) {
     libcellml::Component parent;
     libcellml::Component child;
@@ -10,7 +10,7 @@ TEST(Encapsulation, serialise) {
     const std::string e_parent = "<component/><component/><encapsulation><component_ref><component_ref/></component_ref></encapsulation>";
     std::string a_parent = parent.serialise(libcellml::CELLML_FORMAT_XML);
     EXPECT_EQ(e_parent, a_parent);
-    const std::string e_child= "<component/>";
+    const std::string e_child = "<component/>";
     std::string a_child = child.serialise(libcellml::CELLML_FORMAT_XML);
     EXPECT_EQ(e_child, a_child);
 }
@@ -19,8 +19,8 @@ TEST(Encapsulation, serialise_with_names) {
     libcellml::Component parent;
     parent.setName("parent_component");
     libcellml::Component child;
-    parent.addComponent(child);
     child.setName("child_component");
+    parent.addComponent(child);
     const std::string e_parent = "<component name=\"parent_component\"/><component name=\"child_component\"/><encapsulation><component_ref component=\"parent_component\"><component_ref component=\"child_component\"/></component_ref></encapsulation>";
     std::string a_parent = parent.serialise(libcellml::CELLML_FORMAT_XML);
     EXPECT_EQ(e_parent, a_parent);
@@ -33,7 +33,7 @@ TEST(Encapsulation, reparent_component) {
     libcellml::Component parent;
     parent.setName("parent_component");
     libcellml::Component child1;
-    child1.setName("child2");
+    child1.setName("child1");
     libcellml::Component child2;
     child2.setName("child2");
     libcellml::Component child3;
@@ -45,18 +45,20 @@ TEST(Encapsulation, reparent_component) {
     std::string a_parent = parent.serialise(libcellml::CELLML_FORMAT_XML);
     EXPECT_EQ(e_parent, a_parent);
 
-    // what do we expect this to achieve?
+    // what do we expect this to achieve? Nothing, since we don't add this state of 'child2' to anything?
     child2.addComponent(child3);
 
-    // option 1: add child3 as a child of child2 and leave child3 as also a child of parent_component
-    const std::string e_reparent_option1 = "<component name=\"parent_component\"/><component name=\"child1\"/><component name=\"child2\"/><component name=\"child3\"/><encapsulation><component_ref component=\"parent_component\"><component_ref component=\"child1\"/><component_ref component=\"child2\"><component_ref component=\"child3\"/></component_ref><component_ref component=\"child3\"/></component_ref></encapsulation>";
     a_parent = parent.serialise(libcellml::CELLML_FORMAT_XML);
-    EXPECT_EQ(e_reparent_option1, a_parent);
+    EXPECT_EQ(e_parent, a_parent);
+
+    // Now we have two 'child2's and two 'child3's with a hierarchical encapsulation
+    parent.addComponent(child2);
+    a_parent = parent.serialise(libcellml::CELLML_FORMAT_XML);
+    const std::string e_re_add = "<component name=\"parent_component\"/><component name=\"child1\"/><component name=\"child2\"/><component name=\"child3\"/><component name=\"child2\"/><component name=\"child3\"/><encapsulation><component_ref component=\"parent_component\"><component_ref component=\"child1\"/><component_ref component=\"child2\"/><component_ref component=\"child3\"/><component_ref component=\"child2\"><component_ref component=\"child3\"/></component_ref></component_ref></encapsulation>";
+    EXPECT_EQ(e_re_add, a_parent);
 
     // option 2: add child3 as a child of child2 and remove it as a child of parent_component
-    const std::string e_reparent_option2 = "<component name=\"parent_component\"/><component name=\"child1\"/><component name=\"child2\"/><component name=\"child3\"/><encapsulation><component_ref component=\"parent_component\"><component_ref component=\"child1\"/><component_ref component=\"child2\"><component_ref component=\"child3\"/></component_ref></component_ref></encapsulation>";
-    a_parent = parent.serialise(libcellml::CELLML_FORMAT_XML);
-    EXPECT_EQ(e_reparent_option2, a_parent);
+    // Not really an option is it a bit side-effecty
 
     // other options?
 }
