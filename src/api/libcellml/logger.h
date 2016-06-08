a0a5d15c0364bcfdde541aff109892d6dece3f4e
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-06-09 10:03:04 +1200

    Add documentation to logger error classes.

--- src/api/libcellml/logger.h
+++ src/api/libcellml/logger.h
@@ -38,12 +38,38 @@ public:
     Logger(Logger &&rhs); /**< Move constructor */
     Logger& operator=(Logger rhs); /**< Assignment operator */
 
+    /**
+     * @brief Clear the errors from the logger.
+     *
+     * Clear the errors from the logger.
+     */
     void clearErrors();
 
+    /**
+     * @brief addError
+     * @param err
+     */
     void addError(EntityErrorPtr err);
 
+    /**
+     * @brief Get the number of errors.
+     *
+     * Return the number of errors currently stored in the logger.
+     *
+     * @return The number of errors.
+     */
     size_t errorCount() const;
 
+    /**
+     * @brief Get error at index.
+     *
+     * Returns a reference to an error at the index @p index.  If the  index
+     * is not valid a @c std::out_of_range exception is thrown.
+     *
+     * @param index The index of the error to return (zero-based).
+     *
+     * @return A reference to the error at the given index.
+     */
     EntityErrorPtr getError(size_t index);
 
 private:
@@ -53,62 +79,143 @@ private:
     LoggerImpl* mPimpl; /**< Private member to implementation pointer */
 };
 
+/**
+ * @brief The EntityError class
+ *
+ * Base class for errors used with the logger class.
+ */
 class EntityError
 {
 public:
+    /**
+     * @brief Serialise the error message.
+     *
+     * Serialise the error to a @c std::string.
+     *
+     * @return A @c std::string representation of the error.
+     */
     std::string serialise() const { return doSerialisation(); }
 
 protected:
+    /**
+     * @brief Virtual method for serialising error.
+     *
+     * Virtual method for serialising the error to a @c std::string.
+     *
+     * @return @c std::string representation of the object.
+     */
     virtual std::string doSerialisation() const;
 };
 
+/**
+ * @brief The NamedEntityError class
+ *
+ * NamedEntityError class for all nameable loggable errors.
+ */
 class NamedEntityError: public EntityError
 {
 public:
+    /**
+     * @brief Set the name for the error.
+     *
+     * Method to set the name attribute of the error.
+     *
+     * @param name A @c std::string to represent the name.
+     */
     void setName(const std::string& name) { mName = name; }
+
+    /**
+     * @brief Get the name of the error.
+     *
+     * Method to get the name attribute of the error.
+     *
+     * @return  @c std::string representation of the error.
+     */
     std::string getName() const { return mName; }
 
 private:
-    std::string mName;
+    std::string mName; /**< Name attribute represented as a @c std::string. */
 };
 
+/**
+ * @brief The ImportError class.
+ *
+ * ImportError class for all import loggable errors.
+ */
 class ImportError: public EntityError
 {
 
 };
 
+/**
+ * @brief The VariableError class.
+ *
+ * VariableError class for all variable loggable errors.
+ */
 class VariableError: public NamedEntityError
 {
 public:
+    /**
+     * @brief Set the component for the VariableError.
+     *
+     * Set the component for the VariableError.
+     *
+     * @param c A pointer to the component to set.
+     */
     void setComponent(ComponentPtr c) { mComponent = c; }
 
 protected:
-    virtual std::string doSerialisation() const;
+    virtual std::string doSerialisation() const; /**< Virtual override method for doing serialisation. */
 
 private:
-    ComponentPtr mComponent;
+    ComponentPtr mComponent; /**< Pointer to the component. */
 };
 
+/**
+ * @brief The ImportedEntityError class.
+ *
+ * The ImportedEntityError class.
+ */
 class ImportedEntityError: public NamedEntityError
 {
 
 };
 
+/**
+ * @brief The UnitsError class.
+ *
+ * The UnitsError class.
+ */
 class UnitsError: public ImportedEntityError
 {
 
 };
 
+/**
+ * @brief The ComponentEntityError class.
+ *
+ * The ComponentEntityError class.
+ */
 class ComponentEntityError: public ImportedEntityError
 {
 
 };
 
+/**
+ * @brief The ComponentError class.
+ *
+ * The ComponentError class.
+ */
 class ComponentError: public ComponentEntityError
 {
 
 };
 
+/**
+ * @brief The ModelError class.
+ *
+ * The ModelError class.
+ */
 class ModelError: public ComponentEntityError
 {
 
