7610eb3883e95a100d89e53ad3c696270e735f22
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-26 14:51:48 +1200

    Add canConvertToDouble function.

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,6 +1,38 @@
+/*
+Copyright libCellML Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #include "utilities.h"
 
-utilities::utilities()
+namespace libcellml {
+
+bool canConvertToDouble(const std::string &candidate)
 {
+    bool canConvert = false;
+    // Try to convert the input string to double.
+    try
+    {
+        std::stod(candidate);
+        canConvert = true;
+    } catch (std::invalid_argument) {
+        canConvert = false;
+    } catch (std::out_of_range) {
+        canConvert = false;
+    }
+
+    return canConvert;
+}
 
 }
--- src/utilities.h
+++ src/utilities.h
@@ -1,11 +1,27 @@
-#ifndef UTILITIES_H
-#define UTILITIES_H
+/*
+Copyright libCellML Contributors
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-class utilities
-{
-public:
-    utilities();
-};
+    http://www.apache.org/licenses/LICENSE-2.0
 
-#endif // UTILITIES_H
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <string>
+
+#include "libcellml/exportdefinitions.h"
+
+namespace libcellml {
+
+bool EXPORT_FOR_TESTING canConvertToDouble(const std::string &candidate);
+
+}
