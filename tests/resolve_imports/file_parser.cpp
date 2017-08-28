cd9d2d0cc050981322528523df461a30b8da70bc
David Nickerson <nickerso@users.sourceforge.net>
2017-08-28 14:26:15 +1200

    Adding inital test code for test import resolution.
    Starting work on a test to explore resolving of imports.

---
+++ tests/resolve_imports/file_parser.cpp
@@ -0,0 +1,59 @@
+/*
+Copyright libCellML Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "gtest/gtest.h"
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <libcellml>
+#include <sstream>
+#include <vector>
+
+// generated with test resource locations
+#include "test_resources.h"
+
+static int countImportedComponents(libcellml::ModelPtr model)
+{
+    return -1;
+}
+
+TEST(ResolveImports, parseSineModelFromFile) {
+    std::ifstream t(TestResources::getLocation(
+                    TestResources::CELLML_SINE_MODEL_RESOURCE));
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+
+    libcellml::Parser p(libcellml::Format::XML);
+    libcellml::ModelPtr model = p.parseModel(buffer.str());
+
+    EXPECT_EQ(0, p.errorCount());
+
+    EXPECT_EQ(0, countImportedComponents(model));
+}
+
+TEST(ResolveImports, parseSineImportsModelFromFile) {
+    std::ifstream t(TestResources::getLocation(
+                    TestResources::CELLML_SINE_IMPORTS_MODEL_RESOURCE));
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+
+    libcellml::Parser p(libcellml::Format::XML);
+    libcellml::ModelPtr model = p.parseModel(buffer.str());
+    EXPECT_EQ(0, p.errorCount());
+
+    EXPECT_EQ(3, countImportedComponents(model));
+}
---
+++ tests/resolve_imports/tests.cmake
@@ -0,0 +1,25 @@
+
+# Set the test name, 'test_' will be prepended to the
+# name set here
+set(CURRENT_TEST resolve_imports)
+# Set a category name to enable running commands like:
+#    ctest -R <category-label>
+# which will run the tests matching this category-label.
+# Can be left empty (or just not set)
+set(${CURRENT_TEST}_CATEGORY io)
+list(APPEND LIBCELLML_TESTS ${CURRENT_TEST})
+# Using absolute path relative to this file
+set(${CURRENT_TEST}_SRCS
+  ${CMAKE_CURRENT_LIST_DIR}/file_parser.cpp
+)
+#set(${CURRENT_TEST}_HDRS
+#  ${CMAKE_CURRENT_LIST_DIR}/<test_header_files.h>
+#)
+
+
+set(CELLML_SINE_MODEL_RESOURCE "${CMAKE_CURRENT_SOURCE_DIR}/resources/sine_approximations.xml")
+set(CELLML_SINE_IMPORTS_MODEL_RESOURCE "${CMAKE_CURRENT_SOURCE_DIR}/resources/sine_approximations_import.xml")
+set(CELLML_ORD_MODEL_RESOURCE "${CMAKE_CURRENT_SOURCE_DIR}/resources/Ohara_Rudy_2011.cellml")
+set(CELLML_INVALID_MODEL_RESOURCE "${CMAKE_CURRENT_SOURCE_DIR}/resources/invalid_cellml_2.0.xml")
+
+
