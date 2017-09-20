a35a4fd347c59faeec2d10569ae869bfc58aa580
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-09-20 22:08:48 +0100

    Added initial test_units.py

---
+++ tests/bindings/python/test_units.py
@@ -0,0 +1,71 @@
+#
+# Tests the Units class bindings
+#
+import sys
+import unittest
+
+class UnitsTestCase(unittest.TestCase):
+
+    def test_model(self):
+        from libcellml.units import Units
+        
+        # Test create/copy/destroy
+        x = Units()
+        del(x)
+        y = Units()
+        z = Units(y)
+        del(y,z)
+        
+        # Test inheritance
+        x = Units()
+        from libcellml.importedentity import ImportedEntity
+        self.assertIsInstance(x, ImportedEntity)
+        from libcellml.namedentity import NamedEntity
+        self.assertIsInstance(x, NamedEntity)
+        from libcellml.entity import Entity
+        self.assertIsInstance(x, Entity)
+        
+        # Test access to inherited methods
+        x = Units()
+        idx = 'test'
+        self.assertEqual(x.getId(), '')
+        x.setId(idx)
+        self.assertEqual(x.getId(), idx)
+        y = Units(x)
+        self.assertEqual(y.getId(), idx)
+        del(x, y)
+        
+        # Test own methods
+        #bool isBaseUnit()
+        # void addUnit(const std::string &reference, const std::string &prefix,
+        #   double exponent=1.0, double multiplier=1.0)
+        # void addUnit(const std::string &reference, Prefix prefix,
+        #   double exponent=1.0, double multiplier=1.0)
+        # void addUnit(const std::string &reference, double prefix,
+        #   double exponent, double multiplier=1.0)
+        # void addUnit(const std::string &reference, double exponent)
+        # void addUnit(const std::string &reference)
+        # void addUnit(StandardUnit standardRef, const std::string &prefix,
+        #   double exponent=1.0, double multiplier=1.0)
+        # void addUnit(StandardUnit standardRef, Prefix prefix,
+        #   double exponent=1.0, double multiplier=1.0)
+        # void addUnit(StandardUnit standardRef, double prefix,
+        #   double exponent, double multiplier=1.0)
+        # void addUnit(StandardUnit standardRef, double exponent)
+        # void addUnit(StandardUnit standardRef)
+        # void getUnitAttributes(size_t index, std::string& reference,
+        #   std::string &prefix, double &exponent, double &multiplier)
+        # void getUnitAttributes(const std::string &reference,
+        #   std::string &prefix, double &exponent, double &multiplier)
+        # void getUnitAttributes(StandardUnit standardRef, std::string &prefix,
+        #   double &exponent, double &multiplier)
+        # bool removeUnit(size_t index)
+        # bool removeUnit(const std::string &reference)
+        # bool removeUnit(StandardUnit standardRef)
+        # void removeAllUnits()
+        # void setSourceUnits(const ImportPtr &imp, const std::string &name)
+        # size_t unitCount()
+
+
+if __name__ == '__main__':
+    unittest.main()
