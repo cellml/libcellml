c4afdb5e3fee86cc7d5c8459d027bf63a80d84ac
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-09-22 14:25:20 +0100

    Bindings: Fixed naming of converted enums

--- tests/bindings/python/test_units.py
+++ tests/bindings/python/test_units.py
@@ -33,12 +33,51 @@ class UnitsTestCase(unittest.TestCase):
         self.assertEqual(y.getId(), idx)
         del(x, y)
         
-        # Test built-in units
-        #TODO
+        # Test standard units
+        u = Units()
+        u.addUnit(Units.StandardUnit.AMPERE)
+        u.addUnit(Units.StandardUnit.BECQUEREL)
+        u.addUnit(Units.StandardUnit.CANDELA)
+        u.addUnit(Units.StandardUnit.COULOMB)
+        u.addUnit(Units.StandardUnit.DIMENSIONLESS)
+        u.addUnit(Units.StandardUnit.FARAD)
+        u.addUnit(Units.StandardUnit.GRAM)
+        u.addUnit(Units.StandardUnit.GRAY)
+        u.addUnit(Units.StandardUnit.HENRY)
+        u.addUnit(Units.StandardUnit.HERTZ)
+        u.addUnit(Units.StandardUnit.JOULE)
+        u.addUnit(Units.StandardUnit.KATAL)
+        u.addUnit(Units.StandardUnit.KELVIN)
+        u.addUnit(Units.StandardUnit.KILOGRAM)
+        u.addUnit(Units.StandardUnit.LITER)
+        u.addUnit(Units.StandardUnit.LITRE)
+        u.addUnit(Units.StandardUnit.LUMEN)
+        u.addUnit(Units.StandardUnit.LUX)
+        u.addUnit(Units.StandardUnit.METER)
+        u.addUnit(Units.StandardUnit.METRE)
+        u.addUnit(Units.StandardUnit.MOLE)
+        u.addUnit(Units.StandardUnit.NEWTON)
+        u.addUnit(Units.StandardUnit.OHM)
+        u.addUnit(Units.StandardUnit.PASCAL)
+        u.addUnit(Units.StandardUnit.RADIAN)
+        u.addUnit(Units.StandardUnit.SECOND)
+        u.addUnit(Units.StandardUnit.SIEMENS)
+        u.addUnit(Units.StandardUnit.SIEVERT)
+        u.addUnit(Units.StandardUnit.STERADIAN)
+        u.addUnit(Units.StandardUnit.TESLA)
+        u.addUnit(Units.StandardUnit.VOLT)
+        u.addUnit(Units.StandardUnit.WATT)
+        u.addUnit(Units.StandardUnit.WEBER)
         
         # Test own methods
         
-        #bool isBaseUnit()
+        # bool isBaseUnit()
+        u = Units()
+        self.assertTrue(u.isBaseUnit())
+        u.addUnit(Units.StandardUnit.NEWTON)
+        self.assertFalse(u.isBaseUnit())
+        
+        
         # void addUnit(const std::string &reference, const std::string &prefix,
         #   double exponent=1.0, double multiplier=1.0)
         # void addUnit(const std::string &reference, Prefix prefix,
@@ -55,6 +94,11 @@ class UnitsTestCase(unittest.TestCase):
         #   double exponent, double multiplier=1.0)
         # void addUnit(StandardUnit standardRef, double exponent)
         # void addUnit(StandardUnit standardRef)
+        
+        # Test built-in units
+        #TODO
+
+        
         # void getUnitAttributes(size_t index, std::string& reference,
         #   std::string &prefix, double &exponent, double &multiplier)
         # void getUnitAttributes(const std::string &reference,
