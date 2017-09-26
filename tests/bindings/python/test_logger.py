aa4525a68a59abbe281ae3dbc5a7e4bcd41c8ccb
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2017-09-26 23:00:48 +0100

    [bindings] Added tests and interface for logger

--- tests/bindings/python/test_logger.py
+++ tests/bindings/python/test_logger.py
@@ -18,15 +18,44 @@ class LoggerTestCase(unittest.TestCase):
         del(y, z)
         
         # Test methods
-        #TODO
         
-        # void clearErrors()
-
         # void addError(const ErrorPtr error)
+        from libcellml import Error
+        x = Logger()
+        x.addError(Error())
+        del(x)
 
         # size_t errorCount()
-
+        x = Logger()
+        self.assertEqual(x.errorCount(), 0)
+        x.addError(Error())
+        self.assertEqual(x.errorCount(), 1)
+        x.addError(Error())
+        self.assertEqual(x.errorCount(), 2)
+        del(x)
+        
         # ErrorPtr getError(size_t index)
+        x = Logger()
+        self.assertIsNone(x.getError(0))
+        self.assertIsNone(x.getError(1))
+        self.assertIsNone(x.getError(-1))
+        e = Error()
+        e.setKind(Error.Kind.MODEL)
+        x.addError(e)
+        self.assertIsNotNone(x.getError(0))
+        self.assertIsNone(x.getError(1))
+        self.assertEqual(x.getError(0).getKind(), Error.Kind.MODEL)
+        del(x, e)
+        
+        # void clearErrors()
+        x = Logger()
+        self.assertEqual(x.errorCount(), 0)
+        x.addError(Error())
+        x.addError(Error())
+        self.assertEqual(x.errorCount(), 2)
+        x.clearErrors()
+        self.assertEqual(x.errorCount(), 0)
+        del(x)
 
 if __name__ == '__main__':
     unittest.main()
