ebff877637435d2f3de281d9065b01844b9ded4e
Hugh Sorby <h.sorby@auckland.ac.nz>
2018-05-04 10:32:09 +1200

    Improve documentation.

--- src/reset.cpp
+++ src/reset.cpp
@@ -31,7 +31,7 @@ namespace libcellml {
 struct Reset::ResetImpl
 {
     int mOrder = 0; /**< An integer for determining relative order.*/
-    VariablePtr mVariable; /**< .*/
+    VariablePtr mVariable; /**< The associated variable for the reset.*/
     std::vector<WhenPtr>::iterator findWhen(const WhenPtr &when);
     std::vector<WhenPtr> mWhens;
 };
