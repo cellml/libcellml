87a0136cd05f5c5ff24e7fd1764a9d8a27a65462
Alan Garny <agarny@hellix.com>
2019-03-28 11:27:30 -0700

    Generator: added the skeleton of the algebraic_eqn_derivative_on_rhs_one_component.cellml test.

--- tests/generator/generator.cpp
+++ tests/generator/generator.cpp
@@ -14,8 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include "test_resources.h"
+
 #include "gtest/gtest.h"
 
+#include <fstream>
 #include <libcellml>
 
 TEST(Generator, emptyModel) {
@@ -23,6 +26,31 @@ TEST(Generator, emptyModel) {
 
     libcellml::Generator generator;
     libcellml::ModelPtr model = std::make_shared<libcellml::Model>();
+
+    const std::string a = generator.generateCode(model);
+
+    EXPECT_EQ(e, a);
+
+    EXPECT_EQ(size_t(0), generator.constantCount());
+    EXPECT_EQ(size_t(0), generator.stateCount());
+    EXPECT_EQ(size_t(0), generator.rateCount());
+    EXPECT_EQ(size_t(0), generator.algebraicCount());
+}
+
+TEST(Generator, algebraic_eqn_derivative_on_rhs_one_component) {
+    std::ifstream file(TestResources::getResourcePath("generator/resources/algebraic_eqn_derivative_on_rhs_one_component.cellml"));
+    std::stringstream buffer;
+
+    buffer << file.rdbuf();
+
+    libcellml::Parser parser;
+    libcellml::ModelPtr model = parser.parseModel(buffer.str());
+
+    EXPECT_EQ(size_t(0), parser.errorCount());
+
+    const std::string e = "";
+
+    libcellml::Generator generator;
 
     const std::string a = generator.generateCode(model);
 
