b0e5556f4e5b6445a75b3edb4f5b1accbcfdd10b
Alan Garny <agarny@hellix.com>
2019-05-31 12:54:00 +1200

    Generator test: some minor cleaning up.

--- tests/generator/generator.cpp
+++ tests/generator/generator.cpp
@@ -50,20 +50,12 @@ TEST(Generator, coverage) {
 //      everything indeed.
     libcellml::Parser parser;
     libcellml::ModelPtr model = parser.parseModel(fileContents("generator/resources/coverage.cellml"));
-//libcellml::ModelPtr model = parser.parseModel(fileContents("../../../../Desktop/hodgkin_huxley_squid_axon_model_1952.cellml"));
-//libcellml::ModelPtr model = parser.parseModel(fileContents("../../../../Desktop/noble_model_1962.cellml"));
-//libcellml::ModelPtr model = parser.parseModel(fileContents("../../../../Desktop/van_der_pol_model_1928.cellml"));
-//libcellml::ModelPtr model = parser.parseModel(fileContents("../../../../Desktop/zhang_SAN_model_2000_all.cellml"));
-for (size_t i = 0; i < parser.errorCount(); ++i)
-    std::cout << "Parser error #" << i+1 << ": " << parser.getError(i)->getDescription() << std::endl;
 
     EXPECT_EQ(size_t(0), parser.errorCount());
 
     libcellml::Generator generator;
 
     generator.processModel(model);
-for (size_t i = 0; i < generator.errorCount(); ++i)
-    std::cout << "Generator error #" << i+1 << ": " << generator.getError(i)->getDescription() << std::endl;
 
     EXPECT_EQ(size_t(0), generator.errorCount());
 }
@@ -197,3 +189,20 @@ TEST(Generator, algebraic_eqn_derivative_on_rhs_one_component) {
               generator.computeAlgebraicEquations());
 }
 */
+
+//TODO: remove the below test once we are done testing things...
+TEST(Generator, test) {
+    libcellml::Parser parser;
+//libcellml::ModelPtr model = parser.parseModel(fileContents("../../../../Desktop/hodgkin_huxley_squid_axon_model_1952.cellml"));
+//libcellml::ModelPtr model = parser.parseModel(fileContents("../../../../Desktop/noble_model_1962.cellml"));
+libcellml::ModelPtr model = parser.parseModel(fileContents("../../../../Desktop/van_der_pol_model_1928.cellml"));
+//libcellml::ModelPtr model = parser.parseModel(fileContents("../../../../Desktop/zhang_SAN_model_2000_all.cellml"));
+
+    EXPECT_EQ(size_t(0), parser.errorCount());
+
+    libcellml::Generator generator;
+
+    generator.processModel(model);
+
+    EXPECT_EQ(size_t(0), generator.errorCount());
+}
