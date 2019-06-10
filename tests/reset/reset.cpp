56b54538b8a266aadce5e281ad8f691deca8f0d8
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-06-10 22:06:28 +1200

    Make changes requested in review. Correct heading order. Use const std::string.

--- tests/reset/reset.cpp
+++ tests/reset/reset.cpp
@@ -14,11 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include "test_utils.h"
+
 #include "gtest/gtest.h"
 
 #include <libcellml>
-
-#include "test_utils.h"
 
 TEST(Reset, create)
 {
