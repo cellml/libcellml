8cca18867c454fffef6837ab5430bb0e1419443f
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-06-10 14:07:37 +1200

    Another clang-tidy fix, pass const string by reference.

--- tests/test_utils.h
+++ tests/test_utils.h
@@ -7,7 +7,7 @@
 void printErrors(const libcellml::Validator &v);
 void printErrors(const libcellml::Parser &p);
 
-libcellml::ModelPtr createModel(const std::string name = "");
-libcellml::ModelPtr createModelWithComponent(const std::string name = "");
+libcellml::ModelPtr createModel(const std::string &name = "");
+libcellml::ModelPtr createModelWithComponent(const std::string &name = "");
 
 #endif
