3fd3adf1e9ccafeb965be6eb80c3fa64914703db
Keri <keri.moyle@gmail.com>
2019-06-12 12:34:12 +0200

    Revert "Updating to prime repo"
    This reverts commit 391acf26e3cc46b33540fb0c5e31c923d897406b.

--- docs/CMakeFiles/api_docs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/api_docs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/api_docs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
