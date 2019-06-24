edda33adf80e208e5aec7a0750aad1f253e29435
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-06-24 21:14:13 +1200

    Allow Variable to make shared from this.  Prefer to avoid this but currently don't have alternative solution (suggestions welcome).

--- src/api/libcellml/variable.h
+++ src/api/libcellml/variable.h
@@ -29,13 +29,17 @@ namespace libcellml {
  *
  * Class for each variable in libCellML.
  */
+#ifdef SWIG
 class LIBCELLML_EXPORT Variable: public NamedEntity
+#else
+class LIBCELLML_EXPORT Variable: public NamedEntity, public std::enable_shared_from_this<Variable>
+#endif
 {
 public:
     Variable(); /**< Constructor */
     ~Variable() override; /**< Destructor */
     Variable(const Variable &rhs); /**< Copy constructor */
-    Variable(Variable &&rhs) noexcept; /**< Move constructor */
+    Variable(Variable && rhs) noexcept; /**< Move constructor */
     Variable &operator=(Variable rhs); /**< Assignment operator */
 
     /**
@@ -329,7 +333,7 @@ public:
     std::string interfaceType() const;
 
 private:
-    void swap(Variable &rhs); /**< Swap method required for C++ 11 move semantics. */
+    void swap(Variable & rhs); /**< Swap method required for C++ 11 move semantics. */
 
     struct VariableImpl; /**< Forward declaration for pImpl idiom. */
     VariableImpl *mPimpl; /**< Private member to implementation pointer */
