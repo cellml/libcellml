377a3f915a7b9dec84b22f4e565936090a437ca6
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-06-24 21:23:36 +1200

    Satisfy clang-tidy requirements.

--- src/validator.cpp
+++ src/validator.cpp
@@ -1158,9 +1158,9 @@ bool Validator::ValidatorImpl::isModelVariableCycleFree(const ModelPtr &model, s
                 }
             }
             if (useMe) {
-                std::string description = "";
+                std::string description;
                 std::string reverseDescription;
-                std::string separator = "";
+                std::string separator;
 
                 for (VariablePtr &v : list) {
                     auto *parent = static_cast<Component *>(v->parent());
