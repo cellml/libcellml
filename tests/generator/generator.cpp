9da9c5b3f0f5cd9e5d00c8726cd7b09b659cd9c7
Alan Garny <agarny@hellix.com>
2019-07-10 11:58:39 +1200

    Generator: added a test for an ODE with a constant variable on its RHS.

--- tests/generator/generator.cpp
+++ tests/generator/generator.cpp
@@ -584,6 +584,33 @@ TEST(Generator, ode_computed_var_on_rhs_one_component)
     EXPECT_EQ(EMPTY_STRING, generator.computeAlgebraicEquations());
 }
 
+TEST(Generator, ode_const_var_on_rhs)
+{
+    libcellml::Parser parser;
+    libcellml::ModelPtr model = parser.parseModel(fileContents("generator/resources/ode_const_var_on_rhs/model.cellml"));
+
+    EXPECT_EQ(size_t(0), parser.errorCount());
+
+    libcellml::Generator generator;
+
+    generator.processModel(model);
+
+    EXPECT_EQ(size_t(0), generator.errorCount());
+
+    EXPECT_EQ(libcellml::Generator::ModelType::ODE, generator.modelType());
+
+    EXPECT_EQ(size_t(1), generator.stateCount());
+    EXPECT_EQ(size_t(1), generator.variableCount());
+
+    EXPECT_EQ(EMPTY_STRING, generator.neededMathMethods());
+    EXPECT_EQ(fileContents("generator/resources/ode_const_var_on_rhs/initializeVariables.out"),
+              generator.initializeVariables());
+    EXPECT_EQ(EMPTY_STRING, generator.computeConstantEquations());
+    EXPECT_EQ(fileContents("generator/resources/ode_const_var_on_rhs/computeRateEquations.out"),
+              generator.computeRateEquations());
+    EXPECT_EQ(EMPTY_STRING, generator.computeAlgebraicEquations());
+}
+
 TEST(Generator, cellml_mappings_and_encapsulations)
 {
     libcellml::Parser parser;
