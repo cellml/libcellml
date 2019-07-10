e0e04f206dc04a5440dacf2d2ffc74f1bba07cb7
Alan Garny <agarny@hellix.com>
2019-07-10 14:48:44 +1200

    Generator: generate as full a code as possible (WIP).

---
+++ tests/generator/resources/dependent_eqns/code.c
@@ -0,0 +1,15 @@
+void initializeModel(double *states, double *variables)
+{
+    states[0] = 0.0;
+}
+
+void computeRateEquations(double *states, double *rates, double *variables)
+{
+    rates[0] = 1.0;
+}
+
+void computeAlgebraicEquations(double *states, double *rates, double *variables)
+{
+    variables[0] = 2.0*voi;
+    variables[1] = 3.0*variables[0];
+}
