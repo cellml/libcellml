f7b854706b06adff772f32eaaae8ee1313c609d5
Alan Garny <agarny@hellix.com>
2019-07-10 14:30:04 +1200

    Generator: generate as full a code as possible (WIP).

---
+++ tests/generator/resources/algebraic_eqn_derivative_on_rhs_one_component/code.c
@@ -0,0 +1,16 @@
+void initializeModel(double *states, double *variables)
+{
+    states[0] = 1.0;
+
+    variables[0] = 1.0;
+}
+
+void computeRateEquations(double *states, double *rates, double *variables)
+{
+    rates[0] = variables[0];
+}
+
+void computeAlgebraicEquations(double *states, double *rates, double *variables)
+{
+    variables[1] = rates[0];
+}
