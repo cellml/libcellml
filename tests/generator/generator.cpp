f805442eef4b883ea317eeb3291b52bdfe616df1
Alan Garny <agarny@hellix.com>
2019-07-10 14:29:05 +1200

    Generator: generate as full a code as possible (WIP).

--- tests/generator/generator.cpp
+++ tests/generator/generator.cpp
@@ -379,13 +379,7 @@ TEST(Generator, algebraic_eqn_derivative_on_rhs)
     EXPECT_EQ(size_t(1), generator.stateCount());
     EXPECT_EQ(size_t(2), generator.variableCount());
 
-    EXPECT_EQ(fileContents("generator/resources/algebraic_eqn_derivative_on_rhs/initializeVariables.out"),
-              generator.initializeVariables());
-    EXPECT_EQ(EMPTY_STRING, generator.computeConstantEquations());
-    EXPECT_EQ(fileContents("generator/resources/algebraic_eqn_derivative_on_rhs/computeRateEquations.out"),
-              generator.computeRateEquations());
-    EXPECT_EQ(fileContents("generator/resources/algebraic_eqn_derivative_on_rhs/computeAlgebraicEquations.out"),
-              generator.computeAlgebraicEquations());
+    EXPECT_EQ(fileContents("generator/resources/algebraic_eqn_derivative_on_rhs/code.c"), generator.code());
 }
 
 TEST(Generator, algebraic_eqn_derivative_on_rhs_one_component)
