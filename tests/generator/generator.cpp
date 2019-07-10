cbec646be677728a44694b8b154f1b9a328b25fe
Alan Garny <agarny@hellix.com>
2019-07-10 14:42:40 +1200

    Generator: generate as full a code as possible (WIP).

--- tests/generator/generator.cpp
+++ tests/generator/generator.cpp
@@ -547,12 +547,7 @@ TEST(Generator, ode_constant_on_rhs)
     EXPECT_EQ(size_t(1), generator.stateCount());
     EXPECT_EQ(size_t(0), generator.variableCount());
 
-    EXPECT_EQ(fileContents("generator/resources/ode_constant_on_rhs/initializeVariables.out"),
-              generator.initializeVariables());
-    EXPECT_EQ(EMPTY_STRING, generator.computeConstantEquations());
-    EXPECT_EQ(fileContents("generator/resources/ode_constant_on_rhs/computeRateEquations.out"),
-              generator.computeRateEquations());
-    EXPECT_EQ(EMPTY_STRING, generator.computeAlgebraicEquations());
+    EXPECT_EQ(fileContents("generator/resources/ode_constant_on_rhs/code.c"), generator.code());
 }
 
 TEST(Generator, ode_constant_on_rhs_one_component)
@@ -573,12 +568,7 @@ TEST(Generator, ode_constant_on_rhs_one_component)
     EXPECT_EQ(size_t(1), generator.stateCount());
     EXPECT_EQ(size_t(0), generator.variableCount());
 
-    EXPECT_EQ(fileContents("generator/resources/ode_constant_on_rhs_one_component/initializeVariables.out"),
-              generator.initializeVariables());
-    EXPECT_EQ(EMPTY_STRING, generator.computeConstantEquations());
-    EXPECT_EQ(fileContents("generator/resources/ode_constant_on_rhs_one_component/computeRateEquations.out"),
-              generator.computeRateEquations());
-    EXPECT_EQ(EMPTY_STRING, generator.computeAlgebraicEquations());
+    EXPECT_EQ(fileContents("generator/resources/ode_constant_on_rhs_one_component/code.c"), generator.code());
 }
 
 TEST(Generator, ode_multiple_dependent_odes)
