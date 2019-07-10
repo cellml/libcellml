dec29c94a13ec3795b9426a96afffab653ad5de2
Alan Garny <agarny@hellix.com>
2019-07-10 14:42:40 +1200

    Generator: generate as full a code as possible (WIP).

---
+++ tests/generator/resources/ode_constant_on_rhs/code.c
@@ -0,0 +1,9 @@
+void initializeModel(double *states, double *variables)
+{
+    states[0] = 1.0;
+}
+
+void computeRateEquations(double *states, double *rates, double *variables)
+{
+    rates[0] = 1.0;
+}
