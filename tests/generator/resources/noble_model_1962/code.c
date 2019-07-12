8187bd877864f7cc62439e1699c369d825f8d7c1
Alan Garny <agarny@hellix.com>
2019-07-12 12:31:45 +1200

    Generator: get our tests to pass once again.

--- tests/generator/resources/noble_model_1962/code.c
+++ tests/generator/resources/noble_model_1962/code.c
@@ -2,15 +2,15 @@
 
 void initializeModel(double *states, double *variables)
 {
-    states[0] = 0.01;
-    states[1] = 0.8;
-    states[2] = 0.01;
     states[3] = -87.0;
-    variables[0] = -60.0;
+    variables[0] = 12.0;
     variables[1] = 0.075;
-    variables[2] = 12.0;
-    variables[3] = 40.0;
-    variables[4] = 400.0;
+    variables[2] = -60.0;
+    states[0] = 0.01;
+    variables[3] = 400.0;
+    states[1] = 0.8;
+    variables[4] = 40.0;
+    states[2] = 0.01;
 }
 
 void computeConstantEquations(double *variables)
@@ -19,6 +19,13 @@ void computeConstantEquations(double *variables)
 
 void computeRateEquations(double voi, double *states, double *rates, double *variables)
 {
+    variables[6] = pow(states[0], 3.0)*states[1]*variables[3];
+    variables[7] = (variables[6]+0.14)*(states[3]-variables[4]);
+    variables[5] = variables[1]*(states[3]-variables[2]);
+    variables[12] = 1.2*exp((-states[3]-90.0)/50.0)+0.015*exp((states[3]+90.0)/60.0);
+    variables[13] = 1.2*pow(states[2], 4.0);
+    variables[16] = (variables[12]+variables[13])*(states[3]+100.0);
+    rates[3] = -(variables[7]+variables[16]+variables[5])/variables[0];
     variables[8] = 0.1*(-states[3]-48.0)/(exp((-states[3]-48.0)/15.0)-1.0);
     variables[9] = 0.12*(states[3]+8.0)/(exp((states[3]+8.0)/5.0)-1.0);
     rates[0] = variables[8]*(1.0-states[0])-variables[9]*states[0];
@@ -28,13 +35,6 @@ void computeRateEquations(double voi, double *states, double *rates, double *var
     variables[14] = 0.0001*(-states[3]-50.0)/(exp((-states[3]-50.0)/10.0)-1.0);
     variables[15] = 0.002*exp((-states[3]-90.0)/80.0);
     rates[2] = variables[14]*(1.0-states[2])-variables[15]*states[2];
-    variables[5] = variables[1]*(states[3]-variables[0]);
-    variables[6] = pow(states[0], 3.0)*states[1]*variables[4];
-    variables[7] = (variables[6]+0.14)*(states[3]-variables[3]);
-    variables[12] = 1.2*exp((-states[3]-90.0)/50.0)+0.015*exp((states[3]+90.0)/60.0);
-    variables[13] = 1.2*pow(states[2], 4.0);
-    variables[16] = (variables[12]+variables[13])*(states[3]+100.0);
-    rates[3] = -(variables[7]+variables[16]+variables[5])/variables[2];
 }
 
 void computeAlgebraicEquations(double voi, double *states, double *rates, double *variables)
