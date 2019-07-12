471720bed063276bafc80c7c171730707b902952
Alan Garny <agarny@hellix.com>
2019-07-12 12:31:45 +1200

    Generator: get our tests to pass once again.

--- tests/generator/generator.cpp
+++ tests/generator/generator.cpp
@@ -313,8 +313,8 @@ TEST(Generator, unsuitably_constrained)
     EXPECT_EQ(size_t(0), parser.errorCount());
 
     const std::vector<std::string> expectedErrors = {
-        "Variable 'x' in component 'my_component' of model 'my_model' is not computed.",
-        "Variable 'y' in component 'my_component' of model 'my_model' is computed more than once."};
+        "Variable 'y' in component 'my_component' of model 'my_model' is computed more than once.",
+        "Variable 'x' in component 'my_component' of model 'my_model' is not computed."};
 
     libcellml::Generator generator;
 
--- tests/generator/resources/hodgkin_huxley_squid_axon_model_1952/code.c
+++ tests/generator/resources/hodgkin_huxley_squid_axon_model_1952/code.c
@@ -2,26 +2,31 @@
 
 void initializeModel(double *states, double *variables)
 {
-    states[0] = 0.05;
-    states[1] = 0.6;
-    states[2] = 0.325;
     states[3] = 0.0;
-    variables[0] = 0.3;
-    variables[1] = 1.0;
-    variables[2] = 0.0;
-    variables[3] = 36.0;
-    variables[4] = 120.0;
+    variables[0] = 1.0;
+    variables[1] = 0.0;
+    variables[2] = 0.3;
+    variables[3] = 120.0;
+    states[1] = 0.6;
+    states[0] = 0.05;
+    variables[4] = 36.0;
+    states[2] = 0.325;
 }
 
 void computeConstantEquations(double *variables)
 {
-    variables[6] = variables[2]-10.613;
-    variables[8] = variables[2]-115.0;
-    variables[14] = variables[2]+12.0;
+    variables[6] = variables[1]-10.613;
+    variables[8] = variables[1]-115.0;
+    variables[14] = variables[1]+12.0;
 }
 
 void computeRateEquations(double voi, double *states, double *rates, double *variables)
 {
+    variables[5] = ((voi >= 10.0) && (voi <= 10.5))?-20.0:0.0;
+    variables[7] = variables[2]*(states[3]-variables[6]);
+    variables[15] = variables[4]*pow(states[2], 4.0)*(states[3]-variables[14]);
+    variables[9] = variables[3]*pow(states[0], 3.0)*states[1]*(states[3]-variables[8]);
+    rates[3] = -(-variables[5]+variables[9]+variables[15]+variables[7])/variables[0];
     variables[10] = 0.1*(states[3]+25.0)/(exp((states[3]+25.0)/10.0)-1.0);
     variables[11] = 4.0*exp(states[3]/18.0);
     rates[0] = variables[10]*(1.0-states[0])-variables[11]*states[0];
@@ -31,11 +36,6 @@ void computeRateEquations(double voi, double *states, double *rates, double *var
     variables[16] = 0.01*(states[3]+10.0)/(exp((states[3]+10.0)/10.0)-1.0);
     variables[17] = 0.125*exp(states[3]/80.0);
     rates[2] = variables[16]*(1.0-states[2])-variables[17]*states[2];
-    variables[5] = ((voi >= 10.0) && (voi <= 10.5))?-20.0:0.0;
-    variables[7] = variables[0]*(states[3]-variables[6]);
-    variables[9] = variables[4]*pow(states[0], 3.0)*states[1]*(states[3]-variables[8]);
-    variables[15] = variables[3]*pow(states[2], 4.0)*(states[3]-variables[14]);
-    rates[3] = -(-variables[5]+variables[9]+variables[15]+variables[7])/variables[1];
 }
 
 void computeAlgebraicEquations(double voi, double *states, double *rates, double *variables)
