7dca3a4565cd119a2b1750c65c62f9dc990f1681
Alan Garny <agarny@hellix.com>
2019-07-28 23:22:42 -0700

    Generator: added some documentation for the API of our GeneratorVariable and Generator classes.
    Note: we need to do the same for the API of our GeneratorProfile class.

--- src/api/libcellml/generator.h
+++ src/api/libcellml/generator.h
@@ -42,7 +42,22 @@ public:
     GeneratorVariable(GeneratorVariable &&rhs) noexcept; /**< Move constructor */
     GeneratorVariable &operator=(GeneratorVariable rhs); /**< Assignment operator */
 
+    /**
+     * @brief Get the @c Variable for this @c GeneratorVariable.
+     *
+     * Return the @c Variable of this @c GeneratorVariable.
+     *
+     * @return The @c Variable.
+     */
     VariablePtr variable() const;
+
+    /**
+     * @brief Get the @c Type for this @c GeneratorVariable.
+     *
+     * Return the @c Type of this @c GeneratorVariable.
+     *
+     * @return The @c Type.
+     */
     GeneratorVariable::Type type() const;
 
 private:
@@ -72,19 +87,91 @@ public:
     Generator(Generator &&rhs) noexcept; /**< Move constructor */
     Generator &operator=(Generator rhs); /**< Assignment operator */
 
+    /**
+     * @brief Set the @c GeneratorProfile.
+     *
+     * Set the @c GeneratorProfile to be used by this @c Generator.
+     *
+     * @param profile The @c GeneratorProfile to set.
+     */
     void setProfile(const GeneratorProfilePtr &profile);
 
+    /**
+     * @brief Process the @c Model.
+     *
+     * Process the @c Model using this @c Generator.
+     *
+     * @param model The @c Model to process.
+     */
     void processModel(const ModelPtr &model);
 
+    /**
+     * @brief Get the @c ModelType of the @c Model.
+     *
+     * Return the @c ModelType of the @c Model processed by this @c Generator.
+     *
+     * @return The @c ModelType.
+     */
     ModelType modelType() const;
 
+    /**
+     * @brief Get the number of states in the @c Model.
+     *
+     * Return the number of states in the @c Model processed by this
+     * @c Generator.
+     *
+     * @return The number of states.
+     */
     size_t stateCount() const;
+
+    /**
+     * @brief Get the number of variables in the @c Model.
+     *
+     * Return the number of variables in the @c Model processed by this
+     * @c Generator.
+     *
+     * @return The number of variables.
+     */
     size_t variableCount() const;
 
+    /**
+     * @brief Get the variable of integration of the @c Model.
+     *
+     * Return the @c Variable of integration of the @c Model processed by this
+     * @c Generator.
+     *
+     * @return The @c Type.
+     */
     VariablePtr variableOfIntegration() const;
+
+    /**
+     * @brief Get the state at @p index.
+     *
+     * Return the state at the index @p index for the @c Model processed by this
+     * @c Generator.
+     *
+     * @param index The index of the state to return.
+     */
     VariablePtr state(size_t index) const;
+
+    /**
+     * @brief Get the variable at @p index.
+     *
+     * Return the variable at the index @p index for the @c Model processed by
+     * this @c Generator.
+     *
+     * @param index The index of the variable to return.
+     */
     GeneratorVariablePtr variable(size_t index) const;
 
+    /**
+     * @brief Get the code for the @c Model.
+     *
+     * Return the code for the @c Model processed by this @c Generator, using
+     * its @c GeneratorProfile.
+     *
+     * @return The code.
+     */
     std::string code() const;
 
 private:
