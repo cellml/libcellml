1cd036ac65605a1663efb2cfc5b709d712d68be5
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-08-10 17:36:09 +1200

    Make the Python code generation PEP8 compliant.

--- tests/resources/generator/hodgkin_huxley_squid_axon_model_1952/code.py
+++ tests/resources/generator/hodgkin_huxley_squid_axon_model_1952/code.py
@@ -1,15 +1,19 @@
 from math import *
 
-def leqFunc(x, y):
+
+def leq_func(x, y):
     return 1.0 if (x <= y) else 0.0
 
-def geqFunc(x, y):
+
+def geq_func(x, y):
     return 1.0 if (x >= y) else 0.0
 
-def andFunc(x, y):
+
+def and_func(x, y):
     return 1.0 if (bool(x) & bool(y)) else 0.0
 
-def initializeConstants(states, variables):
+
+def initialize_constants(states, variables):
     states[0] = 0.05
     states[1] = 0.6
     states[2] = 0.325
@@ -20,12 +24,14 @@ def initializeConstants(states, variables):
     variables[3] = 36.0
     variables[4] = 120.0
 
-def computeComputedConstants(variables):
+
+def compute_computed_constants(variables):
     variables[6] = variables[2]-10.613
     variables[8] = variables[2]-115.0
     variables[14] = variables[2]+12.0
 
-def computeRates(voi, states, rates, variables):
+
+def compute_rates(voi, states, rates, variables):
     variables[10] = 0.1*(states[3]+25.0)/(exp((states[3]+25.0)/10.0)-1.0)
     variables[11] = 4.0*exp(states[3]/18.0)
     rates[0] = variables[10]*(1.0-states[0])-variables[11]*states[0]
@@ -35,13 +41,14 @@ def computeRates(voi, states, rates, variables):
     variables[16] = 0.01*(states[3]+10.0)/(exp((states[3]+10.0)/10.0)-1.0)
     variables[17] = 0.125*exp(states[3]/80.0)
     rates[2] = variables[16]*(1.0-states[2])-variables[17]*states[2]
-    variables[5] = -20.0 if (andFunc(geqFunc(voi, 10.0), leqFunc(voi, 10.5))) else 0.0
+    variables[5] = -20.0 if and_func(geq_func(voi, 10.0), leq_func(voi, 10.5)) else 0.0
     variables[7] = variables[0]*(states[3]-variables[6])
     variables[15] = variables[3]*pow(states[2], 4.0)*(states[3]-variables[14])
     variables[9] = variables[4]*pow(states[0], 3.0)*states[1]*(states[3]-variables[8])
     rates[3] = -(-variables[5]+variables[9]+variables[15]+variables[7])/variables[1]
 
-def computeVariables(voi, states, rates, variables):
+
+def compute_variables(voi, states, rates, variables):
     variables[7] = variables[0]*(states[3]-variables[6])
     variables[9] = variables[4]*pow(states[0], 3.0)*states[1]*(states[3]-variables[8])
     variables[10] = 0.1*(states[3]+25.0)/(exp((states[3]+25.0)/10.0)-1.0)
@@ -51,3 +58,4 @@ def computeVariables(voi, states, rates, variables):
     variables[15] = variables[3]*pow(states[2], 4.0)*(states[3]-variables[14])
     variables[16] = 0.01*(states[3]+10.0)/(exp((states[3]+10.0)/10.0)-1.0)
     variables[17] = 0.125*exp(states[3]/80.0)
+
--- tests/resources/generator/noble_model_1962/code.py
+++ tests/resources/generator/noble_model_1962/code.py
@@ -1,6 +1,7 @@
 from math import *
 
-def initializeConstants(states, variables):
+
+def initialize_constants(states, variables):
     states[0] = 0.01
     states[1] = 0.8
     states[2] = 0.01
@@ -11,10 +12,12 @@ def initializeConstants(states, variables):
     variables[3] = 40.0
     variables[4] = 400.0
 
-def computeComputedConstants(variables):
+
+def compute_computed_constants(variables):
     pass
 
-def computeRates(voi, states, rates, variables):
+
+def compute_rates(voi, states, rates, variables):
     variables[8] = 0.1*(-states[3]-48.0)/(exp((-states[3]-48.0)/15.0)-1.0)
     variables[9] = 0.12*(states[3]+8.0)/(exp((states[3]+8.0)/5.0)-1.0)
     rates[0] = variables[8]*(1.0-states[0])-variables[9]*states[0]
@@ -32,7 +35,8 @@ def computeRates(voi, states, rates, variables):
     variables[16] = (variables[12]+variables[13])*(states[3]+100.0)
     rates[3] = -(variables[7]+variables[16]+variables[5])/variables[2]
 
-def computeVariables(voi, states, rates, variables):
+
+def compute_variables(voi, states, rates, variables):
     variables[5] = variables[1]*(states[3]-variables[0])
     variables[6] = pow(states[0], 3.0)*states[1]*variables[4]
     variables[7] = (variables[6]+0.14)*(states[3]-variables[3])
@@ -45,3 +49,4 @@ def computeVariables(voi, states, rates, variables):
     variables[14] = 0.0001*(-states[3]-50.0)/(exp((-states[3]-50.0)/10.0)-1.0)
     variables[15] = 0.002*exp((-states[3]-90.0)/80.0)
     variables[16] = (variables[12]+variables[13])*(states[3]+100.0)
+
