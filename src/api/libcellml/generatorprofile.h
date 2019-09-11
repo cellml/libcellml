9409428c74b7cb7598b0a34e4d77812f5a745f07
Alan Garny <agarny@hellix.com>
2019-09-11 17:54:31 +1200

    Generator: added some documentation for returning a created array.

--- src/api/libcellml/generatorprofile.h
+++ src/api/libcellml/generatorprofile.h
@@ -2204,7 +2204,25 @@ public:
      */
     void setVariablesArrayString(const std::string &variablesArrayString);
 
+    /**
+     * @brief Get the @c std::string for returning a created array.
+     *
+     * Return the @c std::string for returning a created array.
+     *
+     * @return The @c std::string for returning a created array.
+     */
     std::string returnCreatedArrayString() const;
+
+    /**
+     * @brief Set the @c std::string for returning a created array.
+     *
+     * Set this @c std::string for returning a created array. To be useful, the
+     * string should contain the <ARRAY_SIZE> tag, which will be replaced with
+     * the size of the array to be created.
+     *
+     * @param returnCreatedArrayString The @c std::string to use for returning
+     * a created array.
+     */
     void setReturnCreatedArrayString(const std::string &returnCreatedArrayString);
 
     std::string createStatesArrayMethodString() const;
