de54c2fda9943aefb43208c796c947baaa495eb6
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-09-15 10:07:40 +1200

    Add modified generator test files.

---
+++ tests/resources/generator/modified/model.h
@@ -0,0 +1,28 @@
+/* The content of this file was generated using the C profile of libCellML 0.2.0. */
+
+#include <stddef.h>
+
+extern const char VERSION[];
+extern const char LIBCELLML_VERSION[];
+
+extern const size_t STATE_COUNT;
+extern const size_t VARIABLE_COUNT;
+
+typedef enum {
+    CONSTANT,
+    COMPUTED_CONSTANT,
+    ALGEBRAIC
+} VariableType;
+
+typedef struct {
+    char name[5];
+    char units[14];
+    char component[13];
+} VariableInfo;
+
+typedef struct {
+    char name[5];
+    char units[14];
+    char component[13];
+    VariableType type;
+} VariableInfoWithType;
