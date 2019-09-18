d3da3daed326036711c9c97ac04b5c3673d2d859
Alan Garny <agarny@hellix.com>
2019-09-18 17:38:06 +1200

    Generator: don't use a raw string comparison anymore to detect a modified profile.
    Indeed, this meant keeping track of the whole C and Python profiles, which is a bit useless. So, instead, we do as before with hash approach, except that this time we use a SHA-1 value instead.

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -154,4 +154,231 @@ bool areEqual(double value1, double value2)
     return std::abs(value1 - value2) < std::numeric_limits<double>::epsilon();
 }
 
+// The below code is used to compute the SHA-1 value of a string, based on the
+// code available at https://github.com/vog/sha1.
+
+static const size_t BLOCK_INTS = 16;
+static const size_t BLOCK_BYTES = BLOCK_INTS * 4;
+
+static void bufferToBlock(const std::string &buffer, uint32_t block[BLOCK_INTS])
+{
+    for (size_t i = 0; i < BLOCK_INTS; ++i) {
+        block[i] = uint32_t((buffer[4 * i + 3] & 0xff)
+                            | (buffer[4 * i + 2] & 0xff) << 8
+                            | (buffer[4 * i + 1] & 0xff) << 16
+                            | (buffer[4 * i + 0] & 0xff) << 24);
+    }
+}
+
+static uint32_t rol(const uint32_t value, const size_t bits)
+{
+    return (value << bits) | (value >> (32 - bits));
+}
+
+static uint32_t blk(const uint32_t block[BLOCK_INTS], const size_t i)
+{
+    return rol(block[(i + 13) & 15] ^ block[(i + 8) & 15] ^ block[(i + 2) & 15] ^ block[i], 1);
+}
+
+static void r0(const uint32_t block[BLOCK_INTS], const uint32_t v, uint32_t &w,
+               const uint32_t x, const uint32_t y, uint32_t &z, const size_t i)
+{
+    z += ((w & (x ^ y)) ^ y) + block[i] + 0x5a827999 + rol(v, 5);
+    w = rol(w, 30);
+}
+
+static void r1(uint32_t block[BLOCK_INTS], const uint32_t v, uint32_t &w,
+               const uint32_t x, const uint32_t y, uint32_t &z, const size_t i)
+{
+    block[i] = blk(block, i);
+    z += ((w & (x ^ y)) ^ y) + block[i] + 0x5a827999 + rol(v, 5);
+    w = rol(w, 30);
+}
+
+static void r2(uint32_t block[BLOCK_INTS], const uint32_t v, uint32_t &w,
+               const uint32_t x, const uint32_t y, uint32_t &z, const size_t i)
+{
+    block[i] = blk(block, i);
+    z += (w ^ x ^ y) + block[i] + 0x6ed9eba1 + rol(v, 5);
+    w = rol(w, 30);
+}
+
+static void r3(uint32_t block[BLOCK_INTS], const uint32_t v, uint32_t &w,
+               const uint32_t x, const uint32_t y, uint32_t &z, const size_t i)
+{
+    block[i] = blk(block, i);
+    z += (((w | x) & y) | (w & x)) + block[i] + 0x8f1bbcdc + rol(v, 5);
+    w = rol(w, 30);
+}
+
+static void r4(uint32_t block[BLOCK_INTS], const uint32_t v, uint32_t &w,
+               const uint32_t x, const uint32_t y, uint32_t &z, const size_t i)
+{
+    block[i] = blk(block, i);
+    z += (w ^ x ^ y) + block[i] + 0xca62c1d6 + rol(v, 5);
+    w = rol(w, 30);
+}
+
+static void transform(uint32_t digest[], uint32_t block[BLOCK_INTS],
+                      uint64_t &transforms)
+{
+    uint32_t a = digest[0];
+    uint32_t b = digest[1];
+    uint32_t c = digest[2];
+    uint32_t d = digest[3];
+    uint32_t e = digest[4];
+
+    r0(block, a, b, c, d, e, 0);
+    r0(block, e, a, b, c, d, 1);
+    r0(block, d, e, a, b, c, 2);
+    r0(block, c, d, e, a, b, 3);
+    r0(block, b, c, d, e, a, 4);
+    r0(block, a, b, c, d, e, 5);
+    r0(block, e, a, b, c, d, 6);
+    r0(block, d, e, a, b, c, 7);
+    r0(block, c, d, e, a, b, 8);
+    r0(block, b, c, d, e, a, 9);
+    r0(block, a, b, c, d, e, 10);
+    r0(block, e, a, b, c, d, 11);
+    r0(block, d, e, a, b, c, 12);
+    r0(block, c, d, e, a, b, 13);
+    r0(block, b, c, d, e, a, 14);
+    r0(block, a, b, c, d, e, 15);
+    r1(block, e, a, b, c, d, 0);
+    r1(block, d, e, a, b, c, 1);
+    r1(block, c, d, e, a, b, 2);
+    r1(block, b, c, d, e, a, 3);
+    r2(block, a, b, c, d, e, 4);
+    r2(block, e, a, b, c, d, 5);
+    r2(block, d, e, a, b, c, 6);
+    r2(block, c, d, e, a, b, 7);
+    r2(block, b, c, d, e, a, 8);
+    r2(block, a, b, c, d, e, 9);
+    r2(block, e, a, b, c, d, 10);
+    r2(block, d, e, a, b, c, 11);
+    r2(block, c, d, e, a, b, 12);
+    r2(block, b, c, d, e, a, 13);
+    r2(block, a, b, c, d, e, 14);
+    r2(block, e, a, b, c, d, 15);
+    r2(block, d, e, a, b, c, 0);
+    r2(block, c, d, e, a, b, 1);
+    r2(block, b, c, d, e, a, 2);
+    r2(block, a, b, c, d, e, 3);
+    r2(block, e, a, b, c, d, 4);
+    r2(block, d, e, a, b, c, 5);
+    r2(block, c, d, e, a, b, 6);
+    r2(block, b, c, d, e, a, 7);
+    r3(block, a, b, c, d, e, 8);
+    r3(block, e, a, b, c, d, 9);
+    r3(block, d, e, a, b, c, 10);
+    r3(block, c, d, e, a, b, 11);
+    r3(block, b, c, d, e, a, 12);
+    r3(block, a, b, c, d, e, 13);
+    r3(block, e, a, b, c, d, 14);
+    r3(block, d, e, a, b, c, 15);
+    r3(block, c, d, e, a, b, 0);
+    r3(block, b, c, d, e, a, 1);
+    r3(block, a, b, c, d, e, 2);
+    r3(block, e, a, b, c, d, 3);
+    r3(block, d, e, a, b, c, 4);
+    r3(block, c, d, e, a, b, 5);
+    r3(block, b, c, d, e, a, 6);
+    r3(block, a, b, c, d, e, 7);
+    r3(block, e, a, b, c, d, 8);
+    r3(block, d, e, a, b, c, 9);
+    r3(block, c, d, e, a, b, 10);
+    r3(block, b, c, d, e, a, 11);
+    r4(block, a, b, c, d, e, 12);
+    r4(block, e, a, b, c, d, 13);
+    r4(block, d, e, a, b, c, 14);
+    r4(block, c, d, e, a, b, 15);
+    r4(block, b, c, d, e, a, 0);
+    r4(block, a, b, c, d, e, 1);
+    r4(block, e, a, b, c, d, 2);
+    r4(block, d, e, a, b, c, 3);
+    r4(block, c, d, e, a, b, 4);
+    r4(block, b, c, d, e, a, 5);
+    r4(block, a, b, c, d, e, 6);
+    r4(block, e, a, b, c, d, 7);
+    r4(block, d, e, a, b, c, 8);
+    r4(block, c, d, e, a, b, 9);
+    r4(block, b, c, d, e, a, 10);
+    r4(block, a, b, c, d, e, 11);
+    r4(block, e, a, b, c, d, 12);
+    r4(block, d, e, a, b, c, 13);
+    r4(block, c, d, e, a, b, 14);
+    r4(block, b, c, d, e, a, 15);
+
+    digest[0] += a;
+    digest[1] += b;
+    digest[2] += c;
+    digest[3] += d;
+    digest[4] += e;
+
+    ++transforms;
+}
+
+std::string sha1(const std::string &string)
+{
+    uint32_t digest[] = {0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0};
+    std::string buffer;
+    uint64_t transforms = 0;
+    std::istringstream is(string);
+
+    while (true) {
+        char sbuf[BLOCK_BYTES];
+
+        is.read(sbuf, long(BLOCK_BYTES - buffer.size()));
+
+        buffer.append(sbuf, size_t(is.gcount()));
+
+        if (buffer.size() != BLOCK_BYTES) {
+            break;
+        }
+
+        uint32_t block[BLOCK_INTS];
+
+        bufferToBlock(buffer, block);
+        transform(digest, block, transforms);
+
+        buffer.clear();
+    }
+
+    uint64_t totalBits = (transforms * BLOCK_BYTES + buffer.size()) * 8;
+
+    buffer += char(0x80);
+
+    size_t origSize = buffer.size();
+
+    while (buffer.size() < BLOCK_BYTES) {
+        buffer += char(0x00);
+    }
+
+    uint32_t block[BLOCK_INTS];
+
+    bufferToBlock(buffer, block);
+
+    if (origSize > BLOCK_BYTES - 8) {
+        transform(digest, block, transforms);
+
+        for (size_t i = 0; i < BLOCK_INTS - 2; i++) {
+            block[i] = 0;
+        }
+    }
+
+    block[BLOCK_INTS - 1] = uint32_t(totalBits);
+    block[BLOCK_INTS - 2] = uint32_t(totalBits >> 32);
+
+    transform(digest, block, transforms);
+
+    std::ostringstream result;
+
+    for (size_t i = 0; i < sizeof(digest) / sizeof(digest[0]); i++) {
+        result << std::hex << std::setfill('0') << std::setw(8);
+        result << digest[i];
+    }
+
+    return result.str();
+}
+
 } // namespace libcellml
