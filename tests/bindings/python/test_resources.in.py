1e4a3096f7025f65a8b64c9abaeb39f7069940e3
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-09-19 23:06:08 +1200

    Add Python interface to Generator classes.

---
+++ tests/bindings/python/test_resources.in.py
@@ -0,0 +1,11 @@
+
+import os
+
+TESTS_RESOURCE_LOCATION = "${TESTS_RESOURCE_LOCATION}"
+
+def file_contents(file_name):
+
+    with open(os.path.join(TESTS_RESOURCE_LOCATION, file_name)) as f:
+        lines = f.read()
+
+    return lines
