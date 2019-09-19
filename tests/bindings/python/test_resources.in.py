d4958d6dda92ac05c28c8309f01be8bd1c14b9ea
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-09-20 07:39:14 +1200

    Change name of variable in file_contents.

--- tests/bindings/python/test_resources.in.py
+++ tests/bindings/python/test_resources.in.py
@@ -6,6 +6,6 @@ TESTS_RESOURCE_LOCATION = "${TESTS_RESOURCE_LOCATION}"
 def file_contents(file_name):
 
     with open(os.path.join(TESTS_RESOURCE_LOCATION, file_name)) as f:
-        lines = f.read()
+        content = f.read()
 
-    return lines
+    return content
