f1fad4c333771736aa5f65fac785acbc3bc355b7
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-09-29 01:09:51 +1200

    tidy up use of weak pointers.

--- src/entity.cpp
+++ src/entity.cpp
@@ -21,9 +21,7 @@ limitations under the License.
 
 namespace libcellml {
 
-using ModelWeakPtr = std::weak_ptr<Model>; /**< Type definition for weak model pointer. */
-using ComponentWeakPtr = std::weak_ptr<Component>; /**< Type definition for weak component pointer. */
-using EntityWeakPtr = std::weak_ptr<Entity>;
+using EntityWeakPtr = std::weak_ptr<Entity>; /**< Type definition for weak entity pointer. */
 
 /**
  * @brief The Entity::EntityImpl struct.
