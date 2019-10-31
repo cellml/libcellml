2642b08f38169d3d36389b9db35900c54c3642fe
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-10-31 13:58:46 +1300

    Tidy header inclusions in src/entity.cpp.

--- src/entity.cpp
+++ src/entity.cpp
@@ -14,10 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include "libcellml/entity.h"
+
 #include "libcellml/component.h"
 #include "libcellml/componententity.h"
-#include "libcellml/entity.h"
-#include "libcellml/model.h"
 
 namespace libcellml {
 
