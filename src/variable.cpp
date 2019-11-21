025443c642381e9bee01da253e6cfa895896be64
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-11-22 11:32:45 +1300

    Replace redundant boolean literal in conditional return statement in variable.cpp.

--- src/variable.cpp
+++ src/variable.cpp
@@ -227,11 +227,8 @@ VariablePtr Variable::create(const std::string &name) noexcept
 
 bool Variable::addEquivalence(const VariablePtr &variable1, const VariablePtr &variable2)
 {
-    if (variable1 !=nullptr && variable1->mPimpl->setEquivalentTo(variable2) &&
-            variable2 != nullptr && variable2->mPimpl->setEquivalentTo(variable1)) {
-        return true;
-    }
-    return false;
+    return (variable1 !=nullptr && variable1->mPimpl->setEquivalentTo(variable2) &&
+            variable2 != nullptr && variable2->mPimpl->setEquivalentTo(variable1));
 }
 
 bool Variable::addEquivalence(const VariablePtr &variable1, const VariablePtr &variable2, const std::string &mappingId, const std::string &connectionId)
