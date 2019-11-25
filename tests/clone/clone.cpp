e1c04d2d0fdfb6c1f9923c761ec8ca27378de089
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-11-25 15:29:53 +1300

    Progress on cloning a variable.

--- tests/clone/clone.cpp
+++ tests/clone/clone.cpp
@@ -23,16 +23,98 @@ limitations under the License.
 TEST(Clone, variable)
 {
     auto v = libcellml::Variable::create();
+    v->setUnits("lightning");
+    v->setInitialValue(-3.0);
+    v->setInterfaceType(libcellml::Variable::InterfaceType::PRIVATE);
+    v->setId("id_unique");
+    v->setName("variable");
+
     auto vClone = v->clone();
 
+    EXPECT_EQ(nullptr, vClone->parent());
+    EXPECT_NE(v->units(), vClone->units());
+    EXPECT_EQ(v->initialValue(), vClone->initialValue());
+    EXPECT_EQ(v->interfaceType(), vClone->interfaceType());
+    EXPECT_EQ(v->id(), vClone->id());
     EXPECT_EQ(v->name(), vClone->name());
+
+    vClone->setUnits("on_my_own");
+    vClone->setInitialValue("45.0");
+    vClone->setInterfaceType("public");
+    vClone->setId("unique_again");
+    vClone->setName("on_my_own");
+
+    EXPECT_NE(v->units(), vClone->units());
+    EXPECT_EQ(v->initialValue(), "-3");
+    EXPECT_EQ(v->interfaceType(), "private");
+    EXPECT_EQ(v->id(), "id_unique");
+    EXPECT_EQ(v->name(), "variable");
+}
+
+TEST(Clone, variableWithUnitsPtrSet)
+{
+    auto u = libcellml::Units::create();
+    u->setName("daves");
+
+    auto v = libcellml::Variable::create();
+    v->setUnits(u);
+
+    auto vClone = v->clone();
+
+    EXPECT_NE(v->units(), vClone->units());
+    EXPECT_EQ(v->units()->name(), vClone->units()->name());
+    EXPECT_EQ(nullptr, vClone->units()->parent());
+}
+
+TEST(Clone, variableWithEquivalences)
+{
+    auto m = libcellml::Model::create();
+    auto u = libcellml::Units::create();
+    u->setName("daves");
+    m->addUnits(u);
+
+    auto c = libcellml::Component::create();
+    m->addComponent(c);
+
+    auto v = libcellml::Variable::create();
+    v->setUnits(u);
+
+    c->addVariable(v);
+
+    auto vClone = v->clone(true);
+}
+
+TEST(Clone, variableInModel)
+{
+    auto m = libcellml::Model::create();
+    auto u = libcellml::Units::create();
+    u->setName("daves");
+    m->addUnits(u);
+
+    auto c = libcellml::Component::create();
+    m->addComponent(c);
+
+    auto v = libcellml::Variable::create();
+    v->setUnits(u);
+
+    c->addVariable(v);
+
+    auto vClone = v->clone();
+    EXPECT_EQ(nullptr, vClone->parent());
+    EXPECT_NE(v->units(), vClone->units());
+    EXPECT_EQ(nullptr, vClone->units()->parent());
 }
 
 TEST(Clone, component)
 {
     auto c = libcellml::Component::create();
+    c->setName("copy");
+
     auto cClone = c->clone();
 
     EXPECT_EQ(c->name(), cClone->name());
+
+    cClone->setName("on_my_own");
+    EXPECT_NE(c->name(), cClone->name());
 }
 
