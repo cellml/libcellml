5a43c5d2dd7a1bbbeee57da1bb9851e75b4c54cd
Keri Moyle <k.moyle@auckland.ac.nz>
2019-11-27 09:21:44 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -119,7 +119,8 @@ numfig = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = '@SPHINX_THEME@'
+# html_theme = '@SPHINX_THEME@'
+html_theme = 'sphinx_rtd_theme'
 # html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
