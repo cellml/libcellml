0dcff22f66683ab6fa5844e7598575eab9be5a89
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-12-03 08:37:14 +1300

    Use int64_t instead of long.

--- src/model.cpp
+++ src/model.cpp
@@ -595,7 +595,7 @@ IndexStack rebaseIndexStack(const IndexStack &stack, const IndexStack &originSta
     rebasedStack.resize(originStack.size(), SIZE_T_MAX);
     if (rebasedStack == originStack) {
         rebasedStack = destinationStack;
-        auto offsetIt = stack.begin() + static_cast<long>(originStack.size());
+        auto offsetIt = stack.begin() + static_cast<int64_t>(originStack.size());
         rebasedStack.insert(rebasedStack.end(), offsetIt, stack.end());
     } else {
         rebasedStack.clear();
