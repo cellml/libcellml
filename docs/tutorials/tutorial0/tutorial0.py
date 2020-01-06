02536c7228003b830ce494c70f36ba900964ddd2
Keri Moyle <k.moyle@auckland.ac.nz>
2020-01-06 11:42:43 +0100

    Update tutorial0.py
    Missing bracket

--- docs/tutorials/tutorial0/tutorial0.py
+++ docs/tutorials/tutorial0/tutorial0.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
 
     print("--------------------------------------")
     print("    Welcome to libCellML!")
-    print("    This version is {}".format(libcellml.versionString())
+    print("    This version is {}".format(libcellml.versionString()))
     print("--------------------------------------")
