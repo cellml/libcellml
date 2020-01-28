4601e6f1fed40129f61ac5cd6d1b0794b5f3cc7f
Keri Moyle <k.moyle@auckland.ac.nz>
2020-01-28 15:02:46 +0100

    Added tests for resolving circular imports

--- tests/resolve_imports/file_parser.cpp
+++ tests/resolve_imports/file_parser.cpp
@@ -124,3 +124,91 @@ TEST(ResolveImports, componentNotInResolvingModel)
     importer->resolveImports(resourcePath(), model);
     EXPECT_TRUE(model->hasUnresolvedImports());
 }
+
+TEST(ResolveImports, noWarningForkedImport)
+{
+    libcellml::ParserPtr p = libcellml::Parser::create();
+    libcellml::ImporterPtr importer = libcellml::Importer::create();
+    libcellml::ModelPtr model = p->parseModel(fileContents("resolveimports/forkedImport.cellml"));
+
+    EXPECT_EQ(size_t(0), p->errorCount());
+
+    EXPECT_TRUE(model->hasUnresolvedImports());
+    importer->resolveImports(resourcePath("resolveimports/"), model);
+    printIssues(importer);
+    EXPECT_FALSE(model->hasUnresolvedImports());
+}
+
+TEST(ResolveImports, noWarningDiamondImport)
+{
+    libcellml::ParserPtr p = libcellml::Parser::create();
+    libcellml::ImporterPtr importer = libcellml::Importer::create();
+    libcellml::ModelPtr model = p->parseModel(fileContents("resolveimports/diamond.cellml"));
+
+    EXPECT_EQ(size_t(0), p->errorCount());
+
+    EXPECT_TRUE(model->hasUnresolvedImports());
+    importer->resolveImports(resourcePath("resolveimports/"), model);
+    printIssues(importer);
+    EXPECT_FALSE(model->hasUnresolvedImports());
+}
+
+TEST(ResolveImports, warningCircularImportReferencesComponent)
+{
+    std::string warningMessage = "Cyclic dependencies were found when attempting to resolve components in model 'circularImport1'. The dependency loop is:\n"
+                                 "    component 'i_am_cyclic' imports 'c2' from 'circularImport_2.cellml',\n"
+                                 "    component 'c2' imports 'c3' from 'circularImport_3.cellml',\n"
+                                 "    component 'c3' imports 'i_am_cyclic' from 'circularImport_1.cellml',\n"
+                                 "    component 'i_am_cyclic' imports 'c2' from 'circularImport_2.cellml'.";
+    auto parser = libcellml::Parser::create();
+    auto importer = libcellml::Importer::create();
+    auto model = parser->parseModel(fileContents("resolveimports/circularImport_1.cellml"));
+    EXPECT_EQ(size_t(0), parser->issueCount());
+    importer->resolveImports(resourcePath("resolveimports/"), model);
+
+    EXPECT_EQ(size_t(1), importer->issueCount());
+    EXPECT_EQ(size_t(1), importer->warningCount());
+    EXPECT_EQ(warningMessage, importer->warning(0)->description());
+}
+
+TEST(ResolveImports, warningCircularImportReferencesUnits)
+{
+    std::string warningMessage = "Cyclic dependencies were found when attempting to resolve units in model 'circularImport1'. The dependency loop is:\n"
+                                 "    units 'i_am_cyclic' imports 'u2' from 'circularUnits_2.cellml',\n"
+                                 "    units 'u2' imports 'u3' from 'circularUnits_3.cellml',\n"
+                                 "    units 'u3' imports 'i_am_cyclic' from 'circularUnits_1.cellml',\n"
+                                 "    units 'i_am_cyclic' imports 'u2' from 'circularUnits_2.cellml'.";
+    auto parser = libcellml::Parser::create();
+    auto importer = libcellml::Importer::create();
+    auto model = parser->parseModel(fileContents("resolveimports/circularUnits_1.cellml"));
+    EXPECT_EQ(size_t(0), parser->issueCount());
+    importer->resolveImports(resourcePath("resolveimports/"), model);
+    EXPECT_EQ(size_t(1), importer->issueCount());
+    EXPECT_EQ(size_t(1), importer->warningCount());
+    EXPECT_EQ(warningMessage, importer->warning(0)->description());
+}
+
+TEST(ResolveImports, warningUnrequiredCircularDependencyComponent)
+{
+    // This test in intended to show what happens when one model attempts to import a concrete component from a
+    // second model, where the second model has unrelated circular dependencies:
+    //   - model1 imports component1 from model2
+    //   - model2 defines component1
+    //   - model2 also defines a circular dependency unrelated to component1
+
+    std::string warningMessage = "Cyclic dependencies were found when attempting to resolve components in model 'circularImport1'. The dependency loop is:\n"
+                                 "    component 'c' imports 'i_am_ok_but_my_sibling_is_cyclic' from 'circularImport_1.cellml',\n"
+                                 "    component 'i_am_cyclic' imports 'c2' from 'circularImport_2.cellml',\n"
+                                 "    component 'c2' imports 'c3' from 'circularImport_3.cellml',\n"
+                                 "    component 'c3' imports 'i_am_cyclic' from 'circularImport_1.cellml',\n"
+                                 "    component 'i_am_cyclic' imports 'c2' from 'circularImport_2.cellml'.";
+
+    auto parser = libcellml::Parser::create();
+    auto importer = libcellml::Importer::create();
+    auto model = parser->parseModel(fileContents("resolveimports/master1.cellml"));
+    EXPECT_EQ(size_t(0), parser->issueCount());
+    importer->resolveImports(resourcePath("resolveimports/"), model);
+    EXPECT_EQ(size_t(1), importer->issueCount());
+    EXPECT_EQ(size_t(1), importer->warningCount());
+    EXPECT_EQ(warningMessage, importer->warning(0)->description());
+}
