834b6d805f47e539781ebc0449191f9e91a9cc30
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-01-30 08:39:40 +1300

    Only enable debugging functions in the library if explicitly enabled.

--- src/debug.cpp
+++ src/debug.cpp
@@ -13,9 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-#ifdef ENABLE_HANDY_DEBUGGING_FUNCTIONS
-
 #    include "debug.h"
 
 namespace libcellml {
@@ -48,5 +45,3 @@ void printEquivalenceMap(const EquivalenceMap &map)
 }
 
 } // namespace libcellml
-
-#endif /* ENABLE_HANDY_DEBUGGING_FUNCTIONS */
