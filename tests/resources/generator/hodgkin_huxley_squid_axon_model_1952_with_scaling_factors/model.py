e1ed01e62e3ce451ce6ca22f73ef0c38cafbf0dc
Alan Garny <agarny@hellix.com>
2020-02-17 15:51:16 +1300

    Code generation: added a test for connections that need conversion (#409).

---
+++ tests/resources/generator/hodgkin_huxley_squid_axon_model_1952_with_scaling_factors/model.py
@@ -0,0 +1,115 @@
+# The content of this file was generated using the Python profile of libCellML 0.2.0.
+
+from enum import Enum
+from math import *
+
+
+__version__ = "0.1.0"
+LIBCELLML_VERSION = "0.2.0"
+
+STATE_COUNT = 4
+VARIABLE_COUNT = 18
+
+
+class VariableType(Enum):
+    CONSTANT = 1
+    COMPUTED_CONSTANT = 2
+    ALGEBRAIC = 3
+
+
+VOI_INFO = {"name": "time", "units": "millisecond", "component": "membrane"}
+
+STATE_INFO = [
+    {"name": "m", "units": "dimensionless", "component": "sodium_channel_m_gate"},
+    {"name": "h", "units": "dimensionless", "component": "sodium_channel_h_gate"},
+    {"name": "n", "units": "dimensionless", "component": "potassium_channel_n_gate"},
+    {"name": "V", "units": "millivolt", "component": "membrane"}
+]
+
+VARIABLE_INFO = [
+    {"name": "g_L", "units": "milliS_per_cm2", "component": "leakage_current", "type": VariableType.CONSTANT},
+    {"name": "Cm", "units": "microF_per_cm2", "component": "membrane", "type": VariableType.CONSTANT},
+    {"name": "E_R", "units": "millivolt", "component": "membrane", "type": VariableType.CONSTANT},
+    {"name": "g_K", "units": "milliS_per_cm2", "component": "potassium_channel", "type": VariableType.CONSTANT},
+    {"name": "g_Na", "units": "milliS_per_cm2", "component": "sodium_channel", "type": VariableType.CONSTANT},
+    {"name": "E_L", "units": "millivolt", "component": "leakage_current", "type": VariableType.COMPUTED_CONSTANT},
+    {"name": "E_Na", "units": "millivolt", "component": "sodium_channel", "type": VariableType.COMPUTED_CONSTANT},
+    {"name": "E_K", "units": "millivolt", "component": "potassium_channel", "type": VariableType.COMPUTED_CONSTANT},
+    {"name": "i_Stim", "units": "microA_per_cm2", "component": "membrane", "type": VariableType.ALGEBRAIC},
+    {"name": "i_L", "units": "microA_per_cm2", "component": "membrane", "type": VariableType.ALGEBRAIC},
+    {"name": "i_Na", "units": "microA_per_cm2", "component": "membrane", "type": VariableType.ALGEBRAIC},
+    {"name": "alpha_m", "units": "per_millisecond", "component": "sodium_channel_m_gate", "type": VariableType.ALGEBRAIC},
+    {"name": "beta_m", "units": "per_millisecond", "component": "sodium_channel_m_gate", "type": VariableType.ALGEBRAIC},
+    {"name": "alpha_h", "units": "per_millisecond", "component": "sodium_channel_h_gate", "type": VariableType.ALGEBRAIC},
+    {"name": "beta_h", "units": "per_millisecond", "component": "sodium_channel_h_gate", "type": VariableType.ALGEBRAIC},
+    {"name": "i_K", "units": "microA_per_cm2", "component": "membrane", "type": VariableType.ALGEBRAIC},
+    {"name": "alpha_n", "units": "per_millisecond", "component": "potassium_channel_n_gate", "type": VariableType.ALGEBRAIC},
+    {"name": "beta_n", "units": "per_millisecond", "component": "potassium_channel_n_gate", "type": VariableType.ALGEBRAIC}
+]
+
+
+def leq_func(x, y):
+    return 1.0 if x <= y else 0.0
+
+
+def geq_func(x, y):
+    return 1.0 if x >= y else 0.0
+
+
+def and_func(x, y):
+    return 1.0 if bool(x) & bool(y) else 0.0
+
+
+def create_states_array():
+    return [nan]*STATE_COUNT
+
+
+def create_variables_array():
+    return [nan]*VARIABLE_COUNT
+
+
+def initialize_states_and_constants(states, variables):
+    states[0] = 0.05
+    states[1] = 0.6
+    states[2] = 0.325
+    states[3] = 0.0
+    variables[0] = 0.3
+    variables[1] = 1.0
+    variables[2] = 0.0
+    variables[3] = 36000.0
+    variables[4] = 0.12
+
+
+def compute_computed_constants(variables):
+    variables[6] = 1000.0*variables[2]-10.613
+    variables[8] = 1000.0*variables[2]-115.0
+    variables[14] = 1000.0*variables[2]+12.0
+
+
+def compute_rates(voi, states, rates, variables):
+    variables[10] = 0.1*(1000.0*states[3]+25.0)/(exp((1000.0*states[3]+25.0)/10.0)-1.0)
+    variables[11] = 4.0*exp(1000.0*states[3]/18.0)
+    rates[0] = variables[10]*(1.0-states[0])-variables[11]*states[0]
+    variables[12] = 0.07*exp(1000.0*states[3]/20.0)
+    variables[13] = 1.0/(exp((1000.0*states[3]+30.0)/10.0)+1.0)
+    rates[1] = variables[12]*(1.0-states[1])-variables[13]*states[1]
+    variables[16] = 0.01*(1000.0*states[3]+10.0)/(exp((1000.0*states[3]+10.0)/10.0)-1.0)
+    variables[17] = 0.125*exp(1000.0*states[3]/80.0)
+    rates[2] = variables[16]*(1.0-states[2])-variables[17]*states[2]
+    variables[5] = -20.0 if and_func(geq_func(0.001*voi, 0.01), leq_func(0.001*voi, 0.0105)) else 0.0
+    variables[7] = variables[0]*(1000.0*states[3]-variables[6])
+    variables[15] = variables[3]*pow(states[2], 4.0)*(1000.0*states[3]-variables[14])
+    variables[9] = variables[4]*pow(states[0], 3.0)*states[1]*(1000.0*states[3]-variables[8])
+    rates[3] = -0.001*(-variables[5]+1000.0*variables[9]+0.001*variables[15]+variables[7])/variables[1]
+
+
+def compute_variables(voi, states, rates, variables):
+    variables[7] = variables[0]*(1000.0*states[3]-variables[6])
+    variables[9] = variables[4]*pow(states[0], 3.0)*states[1]*(1000.0*states[3]-variables[8])
+    variables[10] = 0.1*(1000.0*states[3]+25.0)/(exp((1000.0*states[3]+25.0)/10.0)-1.0)
+    variables[11] = 4.0*exp(1000.0*states[3]/18.0)
+    variables[12] = 0.07*exp(1000.0*states[3]/20.0)
+    variables[13] = 1.0/(exp((1000.0*states[3]+30.0)/10.0)+1.0)
+    variables[15] = variables[3]*pow(states[2], 4.0)*(1000.0*states[3]-variables[14])
+    variables[16] = 0.01*(1000.0*states[3]+10.0)/(exp((1000.0*states[3]+10.0)/10.0)-1.0)
+    variables[17] = 0.125*exp(1000.0*states[3]/80.0)
