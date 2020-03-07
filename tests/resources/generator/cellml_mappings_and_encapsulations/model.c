bcbc57121cdc79da9c6232cf54631e22742f0c52
Alan Garny <agarny@hellix.com>
2020-03-07 14:05:45 -0800

    Code generator: remove unit scaling from our cellmlMappingsAndEncapsulations test (#409).

--- tests/resources/generator/cellml_mappings_and_encapsulations/model.c
+++ tests/resources/generator/cellml_mappings_and_encapsulations/model.c
@@ -15,7 +15,7 @@ const VariableInfo VOI_INFO = {"t", "ms", "environment"};
 
 const VariableInfo STATE_INFO[] = {
     {"x", "mM", "circle_x"},
-    {"y", "uM", "circle_y_implementation"}
+    {"y", "mM", "circle_y_implementation"}
 };
 
 const VariableInfoWithType VARIABLE_INFO[] = {
@@ -50,12 +50,12 @@ void computeComputedConstants(double *variables)
 
 void computeRates(double voi, double *states, double *rates, double *variables)
 {
-    rates[0] = 0.001*-0.001*states[1]*1.0;
-    rates[1] = 1000.0*states[0]*1.0;
+    rates[0] = -states[1]*1.0;
+    rates[1] = states[0]*1.0;
 }
 
 void computeVariables(double voi, double *states, double *rates, double *variables)
 {
-    variables[0] = 0.001*states[1]+5.0*0.001*states[1]/3.0+1.0*exp(0.001*states[1]/2.0);
-    variables[1] = 2.0*1000.0*states[0];
+    variables[0] = states[1]+5.0*states[1]/3.0+1.0*exp(states[1]/2.0);
+    variables[1] = 2.0*states[0];
 }
