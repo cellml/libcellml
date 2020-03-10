3009e5092b3558bff35f1a8c17599ee89c9119ab
Alan Garny <agarny@hellix.com>
2020-03-10 17:05:41 +0000

    Code generator: some work on the cellmlUnitScaling test (#409).

--- tests/resources/generator/cellml_unit_scaling/model.c
+++ tests/resources/generator/cellml_unit_scaling/model.c
@@ -8,7 +8,7 @@
 const char VERSION[] = "0.1.0";
 const char LIBCELLML_VERSION[] = "0.2.0";
 
-const size_t STATE_COUNT = 5;
+const size_t STATE_COUNT = 7;
 const size_t VARIABLE_COUNT = 3;
 
 const VariableInfo VOI_INFO = {"t", "ms", "environment"};
@@ -17,8 +17,10 @@ const VariableInfo STATE_INFO[] = {
     {"y", "mV", "states"},
     {"x", "mV", "t"},
     {"scaled_x", "volt", "t"},
+    {"z", "mV", "t"},
     {"x", "mV", "scaled_t"},
-    {"scaled_x", "volt", "scaled_t"}
+    {"scaled_x", "volt", "scaled_t"},
+    {"scaled_z", "volt", "scaled_t"}
 };
 
 const VariableInfoWithType VARIABLE_INFO[] = {
@@ -47,8 +49,10 @@ void initializeStatesAndConstants(double *states, double *variables)
     states[0] = 789.0;
     states[1] = 1.0;
     states[2] = 3.0;
-    states[3] = 101.0;
-    states[4] = 103.0;
+    states[3] = 1.23;
+    states[4] = 101.0;
+    states[5] = 103.0;
+    states[6] = 0.001*7.89;
     variables[0] = 123.0;
 }
 
@@ -59,10 +63,12 @@ void computeComputedConstants(double *variables)
 void computeRates(double voi, double *states, double *rates, double *variables)
 {
     rates[0] = 1.23;
-    rates[1] = variables[0]+states[0];
+    rates[1] = variables[0]+states[0]+states[3];
     rates[2] = 0.001*variables[0]+0.001*states[0];
-    rates[3] = 1000.0*(variables[0]+states[0]);
-    rates[4] = 1000.0*(0.001*variables[0]+0.001*states[0]);
+    rates[3] = variables[0]+states[0];
+    rates[4] = 1000.0*(variables[0]+states[0]);
+    rates[5] = 1000.0*(0.001*variables[0]+0.001*states[0]+states[6]);
+    rates[6] = 1000.0*(0.001*variables[0]+0.001*states[0]);
 }
 
 void computeVariables(double voi, double *states, double *rates, double *variables)
