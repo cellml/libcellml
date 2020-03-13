97003b6e097fe4d9f8fdf4ebea66e5b288cc1952
Alan Garny <agarny@hellix.com>
2020-03-13 12:26:34 +0000

    Code generator: isolate the unit scaling test for a constant (#409).

---
+++ tests/resources/generator/cellml_unit_scaling_constant/model.py
@@ -0,0 +1,56 @@
+# The content of this file was generated using the Python profile of libCellML 0.2.0.
+
+from enum import Enum
+from math import *
+
+
+__version__ = "0.1.0"
+LIBCELLML_VERSION = "0.2.0"
+
+STATE_COUNT = 2
+VARIABLE_COUNT = 1
+
+
+class VariableType(Enum):
+    CONSTANT = 1
+    COMPUTED_CONSTANT = 2
+    ALGEBRAIC = 3
+
+
+VOI_INFO = {"name": "t", "units": "ms", "component": "environment"}
+
+STATE_INFO = [
+    {"name": "x", "units": "mM", "component": "main"},
+    {"name": "y", "units": "M", "component": "main"}
+]
+
+VARIABLE_INFO = [
+    {"name": "k", "units": "mM", "component": "constants", "type": VariableType.CONSTANT}
+]
+
+
+def create_states_array():
+    return [nan]*STATE_COUNT
+
+
+def create_variables_array():
+    return [nan]*VARIABLE_COUNT
+
+
+def initialize_states_and_constants(states, variables):
+    states[0] = 3.0
+    states[1] = 5.0
+    variables[0] = 123.0
+
+
+def compute_computed_constants(variables):
+    pass
+
+
+def compute_rates(voi, states, rates, variables):
+    rates[0] = variables[0]
+    rates[1] = 0.001*variables[0]
+
+
+def compute_variables(voi, states, rates, variables):
+    pass
