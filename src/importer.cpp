73c12896ad1dd844117c6ad165068a36f4af2351
Keri Moyle <k.moyle@auckland.ac.nz>
2020-05-28 11:49:38 +1200

    First commit: creating empty class

---
+++ src/importer.cpp
@@ -0,0 +1,67 @@
+/*
+Copyright libCellML Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "libcellml/importer.h"
+
+
+#include <algorithm>
+#include <cmath>
+#include <libxml/uri.h>
+#include <stdexcept>
+
+#include "libcellml/component.h"
+#include "libcellml/importsource.h"
+#include "libcellml/model.h"
+#include "libcellml/reset.h"
+#include "libcellml/units.h"
+#include "libcellml/variable.h"
+
+#include "namespaces.h"
+#include "utilities.h"
+#include "xmldoc.h"
+#include "xmlutils.h"
+
+
+namespace libcellml {
+
+/**
+ * @brief The Importer::ImporterImpl struct.
+ *
+ * This struct is the private implementation struct for the Importer class.  Separating
+ * the implementation from the definition allows for greater flexibility when
+ * distributing the code.
+ */
+struct Importer::ImporterImpl
+{
+
+};
+
+Importer::Importer()
+    : mPimpl(new ImporterImpl())
+{
+}
+
+ImporterPtr Importer::create() noexcept
+{
+    return std::shared_ptr<Importer> {new Importer {}};
+}
+
+Importer::~Importer()
+{
+    delete mPimpl;
+}
+
+} // namespace libcellml
