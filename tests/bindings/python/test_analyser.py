e4b0af6a9cc4ca441fcb139ad79d7ad639148b11
Alan Garny <agarny@hellix.com>
2020-07-01 12:10:11 +1200

    Analyser: added a Python test for testing the Analyser's inheritance.

--- tests/bindings/python/test_analyser.py
+++ tests/bindings/python/test_analyser.py
@@ -12,6 +12,20 @@ class AnalyserTestCase(unittest.TestCase):
         x = Analyser()
         del x
 
+    def test_inheritance(self):
+        import libcellml
+        from libcellml import Analyser
+
+        x = Analyser()
+        self.assertIsInstance(x, libcellml.logger.Logger)
+
+        # Test access to inherited methods
+        self.assertIsNone(x.issue(0))
+        self.assertIsNone(x.issue(-1))
+        self.assertEqual(x.issueCount(), 0)
+        x.addIssue(libcellml.Issue())
+        self.assertEqual(x.issueCount(), 1)
+
     def test_process_model(self):
         from libcellml import Analyser
         from libcellml import AnalyserModel
