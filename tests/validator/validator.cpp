54a571ed8e2076f9b4d413fb4975a50a469c350f
Keri Moyle <k.moyle@auckland.ac.nz>
2020-08-10 08:46:52 +1200

    test to show behaviour: will fail

--- tests/validator/validator.cpp
+++ tests/validator/validator.cpp
@@ -846,6 +846,53 @@ TEST(Validator, validMathMLCiAndCnElementsWithCellMLUnits)
     EXPECT_EQ(size_t(0), v->issueCount());
 }
 
+TEST(Validator, invalidMathMLDegreeOutsideRootOrDiff)
+{
+    const std::string math =
+        "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">\n"
+        "  <apply>\n"
+        "    <eq/>\n"
+        "    <ci>C</ci>\n"
+        "    <apply>\n"
+        "      <degree/>\n"
+        "      <ci>A</ci>\n"
+        "      <ci>B</ci>\n"
+        "    </apply>\n"
+        "  </apply>\n"
+        "</math>\n";
+
+    libcellml::ValidatorPtr v = libcellml::Validator::create();
+    libcellml::ModelPtr m = libcellml::Model::create();
+    libcellml::ComponentPtr c = libcellml::Component::create();
+    libcellml::VariablePtr v1 = libcellml::Variable::create();
+    libcellml::VariablePtr v2 = libcellml::Variable::create();
+    libcellml::VariablePtr v3 = libcellml::Variable::create();
+
+    m->setName("modelName");
+    c->setName("componentName");
+    v1->setName("A");
+    v2->setName("B");
+    v3->setName("C");
+    v1->setInitialValue("1.0");
+    v2->setInitialValue("-1.0");
+    v1->setUnits("dimensionless");
+    v2->setUnits("dimensionless");
+    v3->setUnits("dimensionless");
+
+    c->addVariable(v1);
+    c->addVariable(v2);
+    c->addVariable(v3);
+    c->setMath(math);
+    m->addComponent(c);
+
+    v->validateModel(m);
+
+    // Issue #682: the <degree> tag is in a random place in the MathML, when it's
+    //             only allowed inside a <diff> or <root> tag. Expect something to 
+    //             be reported...
+    EXPECT_NE(size_t(0), v->issueCount());
+}
+
 TEST(Validator, parseAndValidateInvalidUnitIssues)
 {
     const std::string in =
