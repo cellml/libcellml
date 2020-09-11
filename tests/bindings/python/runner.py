0a1c344b0b244a86155e60615eb70ebc56f9b193
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-09-11 12:26:57 +1200

    Create targets 'python_coverage' and 'python_cooverage_html_report' for running Python coverage over Python bindings.

--- tests/bindings/python/runner.py
+++ tests/bindings/python/runner.py
@@ -1,13 +1,26 @@
 import coverage
-import importlib
+import os
+import sys
 import unittest
+
+args = sys.argv[:]
+args.pop(0) # Remove program name.
+
+configuration = ""
+if len(args):
+  configuration = args.pop(0)
+
+arg1 = ""
+if len(args):
+  arg1 = args.pop(0)
 
 suite = unittest.TestLoader().discover(".")
 
 cov = coverage.Coverage(source=["libcellml"])
 cov.set_option("run:plugins", ["exclude_until_coverage_plugin"])
-#cov.set_option("exclude_until_coverage_plugin:marker", "# libCellML generated wrapper code starts here.")
+cov.set_option("exclude_until_coverage_plugin:marker", "# libCellML generated wrapper code starts here.")
 
+os.chdir(os.path.join("@PYTHON_COVERAGE_WORKING_DIRIRECTORY@", configuration))
 cov.start()
 
 unittest.TextTestRunner(verbosity=1).run(suite)
@@ -15,4 +28,7 @@ unittest.TextTestRunner(verbosity=1).run(suite)
 cov.stop()
 cov.save()
 
-cov.report()
+if arg1 == "html":
+  cov.html_report(directory='coverage_html_report')
+else:
+  cov.report()
