bb742dcd49ef89a588b86f622c5d0afa73448e13
Alan Garny <agarny@hellix.com>
2020-11-02 14:32:38 +1300

    Utiltities: put back the method to test whether two variables are equivalent.
    Note, however, that the method used to be called `isSameOrEquivalentVariable()` while it is now called `areEquivalentVariables()` which is more in line with `AnalyserModel::areEquivalentVariables()`.

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -453,6 +453,12 @@ size_t getVariableIndexInComponent(const ComponentPtr &component, const Variable
     return index;
 }
 
+bool areEquivalentVariables(const VariablePtr &variable1,
+                            const VariablePtr &variable2)
+{
+    return (variable1 == variable2) || variable1->hasEquivalentVariable(variable2, true);
+}
+
 bool isEntityChildOf(const EntityPtr &entity1, const EntityPtr &entity2)
 {
     return entity1->parent() == entity2;
