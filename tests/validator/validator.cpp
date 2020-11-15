038954b5553ba82d9dc369b08062367e57a2412b
Keri Moyle <k.moyle@auckland.ac.nz>
2020-11-16 09:40:08 +1300

    Comparing units across connections now skips missing units as they're reported elsewhere.

--- tests/validator/validator.cpp
+++ tests/validator/validator.cpp
@@ -1983,24 +1983,39 @@ TEST(Validator, unitUserCreatedUnitsBananasAndApples)
 
 TEST(Validator, unitIllDefinedEquivalentUnits)
 {
-    const std::vector<std::string> expectedIssues = {
+    const std::vector<std::string> expectedIssues1 = {
         "Variable 'v1' in component 'c1' does not have any units specified.",
         "Variable 'v2' in component 'c2' does not have any units specified.",
-        "Variable 'v1' in component 'c1' has units of '' and an equivalent variable 'v2' in component 'c2' with non-matching units of ''. The mismatch is: ",
+    };
+
+    const std::vector<std::string> expectedIssues2 = {
+        "Variable 'v1' in component 'c1' has units of 'metre' and an equivalent variable 'v3' in component 'c3' with non-matching units of 'second'. The mismatch is: metre^1, second^-1.",
     };
 
     libcellml::ValidatorPtr validator = libcellml::Validator::create();
     libcellml::ModelPtr m = createModelTwoComponentsWithOneVariableEach("m", "c1", "c2", "v1", "v2");
     auto c1 = m->component(0);
     auto c2 = m->component(1);
+
     auto v1 = c1->variable(0);
     auto v2 = c2->variable(0);
 
+    m->addComponent(libcellml::Component::create("c3"));
+    auto v3 = libcellml::Variable::create("v3");
+    m->component("c3")->addVariable(v3);
+    v3->setUnits("second");
+    v3->setInterfaceType("public");
+
     libcellml::Variable::addEquivalence(v1, v2);
+    libcellml::Variable::addEquivalence(v3, v1);
+    validator->validateModel(m);
+    EXPECT_EQ_ISSUES(expectedIssues1, validator);
+
+    v1->setUnits("metre");
+    v2->setUnits("metre");
 
     validator->validateModel(m);
-
-    EXPECT_EQ_ISSUES(expectedIssues, validator);
+    EXPECT_EQ_ISSUES(expectedIssues2, validator);
 }
 
 TEST(Validator, unitStandardUnitsWhichAreBaseUnits)
