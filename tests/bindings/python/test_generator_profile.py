92188f3f4a3dc5cb9a2b81767eb98a62065572e2
Alan Garny <agarny@hellix.com>
2020-12-03 12:33:33 +1300

    Generator: bumped our version to version 0.2.0.

--- tests/bindings/python/test_generator_profile.py
+++ tests/bindings/python/test_generator_profile.py
@@ -869,7 +869,7 @@ class GeneratorProfileTestCase(unittest.TestCase):
 
         g = GeneratorProfile()
 
-        self.assertEqual('const char VERSION[] = "0.1.0";\n', g.implementationVersionString())
+        self.assertEqual('const char VERSION[] = "0.2.0";\n', g.implementationVersionString())
         g.setImplementationVersionString(GeneratorProfileTestCase.VALUE)
         self.assertEqual(GeneratorProfileTestCase.VALUE, g.implementationVersionString())
 
