f309f39a59db23c3c47637ddc77c8d915a3b3788
Keri Moyle <k.moyle@auckland.ac.nz>
2020-12-09 12:05:56 +1300

    remove MAX_SIZE_T from tests

--- tests/test_utils.h
+++ tests/test_utils.h
@@ -27,8 +27,6 @@ limitations under the License.
 #include "../src/commonutils.h"
 #undef TEST_UTILS
 
-static const size_t MAX_SIZE_T = std::numeric_limits<size_t>::max();
-
 const std::string EMPTY_MATH = "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"/>\n";
 
 const std::string NON_EMPTY_MATH =
