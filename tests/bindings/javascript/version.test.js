faf94bfbd3f608428468f0bf5d1eb3e7f7aa0387
hsorby <h.sorby@auckland.ac.nz>
2021-03-01 23:11:27 +1300

    Add a javascript bindings test framework and the beginings of those tests.

---
+++ tests/bindings/javascript/version.test.js
@@ -0,0 +1,11 @@
+const libCellMLModule = require('libcellml.js/libcellml.common')
+let libcellml = null
+
+describe("Version tests", () => {
+  beforeAll(async () => {
+        libcellml = await libCellMLModule();
+    });
+ test('Checking version string.', () => {
+   expect(libcellml.versionString()).toBe('0.2.0');
+ });
+})
