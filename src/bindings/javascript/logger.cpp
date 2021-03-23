b24889857e3873ec028f3cf15274498f2db19745
hsorby <h.sorby@auckland.ac.nz>
2021-03-24 11:55:45 +1300

    Update logger coverage to include all logger API.

--- src/bindings/javascript/logger.cpp
+++ src/bindings/javascript/logger.cpp
@@ -9,14 +9,18 @@ using namespace emscripten;
 EMSCRIPTEN_BINDINGS(libcellml_logger) {
 
     class_<libcellml::Logger>("Logger")
-        .function("addIssue", &libcellml::Logger::addIssue)
         .function("removeAllIssues", &libcellml::Logger::removeAllIssues)
-        .function("error", &libcellml::Logger::error)
-        .function("warning", &libcellml::Logger::warning)
-        .function("hint", &libcellml::Logger::hint)
+        .function("addIssue", &libcellml::Logger::addIssue)
+        .function("addIssues", &libcellml::Logger::addIssues)
         .function("issueCount", &libcellml::Logger::issueCount)
+        .function("issue", &libcellml::Logger::issue)
         .function("errorCount", &libcellml::Logger::errorCount)
+        .function("error", &libcellml::Logger::error)
         .function("warningCount", &libcellml::Logger::warningCount)
+        .function("warning", &libcellml::Logger::warning)
         .function("hintCount", &libcellml::Logger::hintCount)
+        .function("hint", &libcellml::Logger::hint)
+        .function("messageCount", &libcellml::Logger::messageCount)
+        .function("message", &libcellml::Logger::message)
     ;
 }
