b5f55fee928a54cb8a141aff6739bd6d7e84bd8a
hsorby <h.sorby@auckland.ac.nz>
2021-03-24 17:15:08 +1300

    Simplify compare import source.

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -347,11 +347,6 @@ void compareComponent(const libcellml::ComponentPtr &c1, const libcellml::Compon
     }
 }
 
-void compareImportSource(const libcellml::ImportSourcePtr &i1, const libcellml::ImportSourcePtr &i2)
-{
-    EXPECT_EQ(i1->url(), i2->url());
-}
-
 void compareModel(const libcellml::ModelPtr &m1, const libcellml::ModelPtr &m2)
 {
     EXPECT_EQ(m1->id(), m2->id());
@@ -376,7 +371,7 @@ void compareModel(const libcellml::ModelPtr &m1, const libcellml::ModelPtr &m2)
     for (size_t index = 0; index < m1->importSourceCount(); ++index) {
         auto i1 = m1->importSource(index);
         auto i2 = m2->importSource(index);
-        compareImportSource(i1, i2);
+        EXPECT_EQ(i1->url(), i2->url());
     }
 }
 
