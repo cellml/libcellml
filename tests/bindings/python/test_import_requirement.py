16c39ca2e72a5ba863bbe137cfdd5adcf020f3fc
hsorby <h.sorby@auckland.ac.nz>
2021-03-26 16:50:19 +1300

    Add importRequirements method to Model class.

--- tests/bindings/python/test_import_requirement.py
+++ tests/bindings/python/test_import_requirement.py
@@ -3,30 +3,10 @@
 #
 import unittest
 
-from test_resources import resource_path, file_contents
+from test_resources import file_contents
 
 
 class ImportRequirementTestCase(unittest.TestCase):
-
-    def test_create_destroy(self):
-        from libcellml.types import ImportRequirement
-        from libcellml import Model
-        m = Model()
-        url = 'i-am-a-url'
-        x = ImportRequirement(url, m)
-        del x
-
-    def test_url_model(self):
-        from libcellml.types import ImportRequirement
-        from libcellml import Model
-
-        m = Model()
-        m.setName('my_model_name')
-        url = 'i-am-a-url'
-        x = ImportRequirement(url, m)
-        self.assertEquals(url, x.url())
-        self.assertEquals('my_model_name', x.model().name())
-        del x
 
     def test_requirements(self):
         from libcellml import Parser, Importer
@@ -39,20 +19,14 @@ class ImportRequirementTestCase(unittest.TestCase):
         ]
 
         parser = Parser()
-        model = parser.parseModel(
-            file_contents('importer/requirements/complicatedExample.cellml'))
+        model = parser.parseModel(file_contents('importer/requirements/complicatedExample.cellml'))
         self.assertEqual(0, parser.issueCount())
 
-        importer = Importer()
-        importer.resolveImports(model, resource_path('importer/requirements/'))
-        self.assertEqual(0, importer.issueCount())
-
-        requirements = importer.requirements(model)
+        requirements = model.importRequirements()
 
         i = 0
         for r in requirements:
-            self.assertEqual(keys[i], r.url())
-            self.assertEqual(importer.library(resource_path('importer/requirements/'+keys[i])).name(), r.model().name())
+            self.assertEqual(keys[i], r)
             i += 1
 
 
