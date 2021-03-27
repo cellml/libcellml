68d0790d200adaf6eaf0b660a138b8e4224249f7
hsorby <h.sorby@auckland.ac.nz>
2021-03-27 21:02:55 +1300

    Modify ulpsDistance calculation to work without warnings.

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -186,9 +186,9 @@ bool areEqual(double a, double b)
     return convertToString(a + 0.0) == convertToString(b + 0.0);
 }
 
-ptrdiff_t ulpsDistance(double a, double b)
+uint64_t ulpsDistance(double a, double b)
 {
-    static const auto max = std::numeric_limits<ptrdiff_t>::max();
+    static const auto max = std::numeric_limits<uint64_t>::max();
 
     // Max distance for NaN.
     if (std::isnan(a) || std::isnan(b)) {
@@ -208,11 +208,12 @@ ptrdiff_t ulpsDistance(double a, double b)
     memcpy(&ib, &b, SIZE_OF_DOUBLE);
 
     // Return the absolute value of the distance in ULPs.
-    ptrdiff_t distance = ia - ib;
-    if (distance < 0) {
-        return -distance;
+    uint64_t distance = max;
+    if (ia < ib) {
+        distance = ib + ~ia + 1;
+    } else {
+        distance = ia + ~ib + 1;
     }
-
     return distance;
 }
 
