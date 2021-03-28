ca421a4904cbfd1d11b775a5aaf8f6d2ef6cf6b7
hsorby <h.sorby@auckland.ac.nz>
2021-03-28 16:22:19 +1300

    Make javascript bindings compile.

--- src/bindings/javascript/entity.cpp
+++ src/bindings/javascript/entity.cpp
@@ -10,12 +10,9 @@ EMSCRIPTEN_BINDINGS(libcellml_entity) {
 
     class_<libcellml::Entity>("Entity")
         .smart_ptr<std::shared_ptr<libcellml::Entity>>("shared_ptr<libcellml::Entity>")
-        .function("id", &libcellml::Entity::id)
         .function("setId", &libcellml::Entity::setId)
-        .function("parent", &libcellml::Entity::parent)
-        .function("setParent", &libcellml::Entity::setParent)
-        .function("removeParent", &libcellml::Entity::removeParent)
-        .function("hasParent", &libcellml::Entity::hasParent)
-        .function("hasAncestor", &libcellml::Entity::hasAncestor)
+        .function("id", &libcellml::Entity::id)
+        .function("removeId", &libcellml::Entity::removeId)
+        .function("equals", &libcellml::Entity::equals)
     ;
 }
