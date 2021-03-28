ff96b6fc76066be430302155712dfe692495d792
hsorby <h.sorby@auckland.ac.nz>
2021-03-28 22:48:02 +1300

    Wrap all the API available in libCellML.

---
+++ src/bindings/javascript/analyserequation.cpp
@@ -0,0 +1,25 @@
+# include <emscripten/bind.h>
+
+# include "libcellml/analyserequation.h"
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(libcellml_analyserequation)
+{
+
+    enum_<libcellml::AnalyserEquation::Type>("Type")
+        .value("TRUE_CONSTANT", libcellml::AnalyserEquation::Type::TRUE_CONSTANT)
+        .value("VARIABLE_BASED_CONSTANT", libcellml::AnalyserEquation::Type::VARIABLE_BASED_CONSTANT)
+        .value("RATE", libcellml::AnalyserEquation::Type::RATE)
+        .value("ALGEBRAIC", libcellml::AnalyserEquation::Type::ALGEBRAIC)
+        .value("EXTERNAL", libcellml::AnalyserEquation::Type::EXTERNAL)
+    ;
+
+    class_<libcellml::AnalyserEquation>("AnalyserEquation")
+        .function("type", &libcellml::AnalyserEquation::type)
+        .function("ast", &libcellml::AnalyserEquation::ast)
+        .function("dependencies", &libcellml::AnalyserEquation::dependencies)
+        .function("isStateRateBased", &libcellml::AnalyserEquation::isStateRateBased)
+        .function("variable", &libcellml::AnalyserEquation::variable)
+    ;
+}
