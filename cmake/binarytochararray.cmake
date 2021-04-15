3f1fbf7f312e2bb7a8f8c6d595b13b40367a6cb9
hsorby <h.sorby@auckland.ac.nz>
2021-04-15 12:21:38 +1200

    Explain current use and implementation of binarytochararray.cmake.

--- cmake/binarytochararray.cmake
+++ cmake/binarytochararray.cmake
@@ -14,9 +14,15 @@
 
 # This script requires CMake version 3.18 to run hence why it is not directly part of the build configuration.
 # usage:
-#  cmake -P binary_to_char_array.cmake <full-path-to-xhtml-math11-f.dtd>
+#  cmake -P binarytochararray.cmake <full-path-to-xhtml-math11-f.dtd>
 #
 # Err, not quite working like that just yet.
+#
+# Currently we have to compress the DTD and then we generate the header and implementation file
+# in the source tree.  We also pass the compressed data and original size of the DTD to this script.
+# It would be prefereble for this not to happen.  Generating the DTD in the build tree is the preferred option
+# but this can't happen until the minimum CMake version can be set to 3.18.
+
 
 function(STRING_HEX_KEY_TO_C_BYTE_ARRAY STRING_HEX _DATA _LEN)
   # Separate into individual bytes.
