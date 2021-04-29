8cd2c58e3745e1a7a51aae33ce8bbc68fa6083a9
Alan Garny <agarny@hellix.com>
2021-04-29 16:50:56 +1200

    Some minor cleaning up.

--- src/enums.cpp
+++ src/enums.cpp
@@ -33,8 +33,8 @@ static const std::map<CellmlElementType, std::string> cellmlElementTypeToString 
     {CellmlElementType::RESET_VALUE, "reset_value"},
     {CellmlElementType::TEST_VALUE, "test_value"},
     {CellmlElementType::UNDEFINED, "undefined"},
-    {CellmlElementType::UNITS, "units"},
     {CellmlElementType::UNIT, "unit"},
+    {CellmlElementType::UNITS, "units"},
     {CellmlElementType::VARIABLE, "variable"}};
 
 std::string cellmlElementTypeAsString(CellmlElementType value)
