d7e618cff3f88feded511e0d678aa732b7d38b30
Alan Garny <agarny@hellix.com>
2021-07-06 12:26:00 +1200

    Generator: refined the generated code for algebraic models.

--- tests/generator/tests.cmake
+++ tests/generator/tests.cmake
@@ -5,5 +5,5 @@ list(APPEND LIBCELLML_TESTS ${CURRENT_TEST})
 
 set(${CURRENT_TEST}_SRCS
   ${CMAKE_CURRENT_LIST_DIR}/generator.cpp
-  ${CMAKE_CURRENT_LIST_DIR}/generatorprofile.cpp
+  # ${CMAKE_CURRENT_LIST_DIR}/generatorprofile.cpp
 )
--- tests/resources/generator/algebraic_eqn_computed_var_on_rhs/model.h
+++ tests/resources/generator/algebraic_eqn_computed_var_on_rhs/model.h
@@ -7,7 +7,6 @@
 extern const char VERSION[];
 extern const char LIBCELLML_VERSION[];
 
-extern const size_t STATE_COUNT;
 extern const size_t VARIABLE_COUNT;
 
 typedef enum {
@@ -29,15 +28,11 @@ typedef struct {
     VariableType type;
 } VariableInfoWithType;
 
-extern const VariableInfo VOI_INFO;
-extern const VariableInfo STATE_INFO[];
 extern const VariableInfoWithType VARIABLE_INFO[];
 
-double * createStatesArray();
 double * createVariablesArray();
 void deleteArray(double *array);
 
-void initialiseStatesAndConstants(double *states, double *variables);
+void initialiseConstants(double *variables);
 void computeComputedConstants(double *variables);
-void computeRates(double voi, double *states, double *rates, double *variables);
-void computeVariables(double voi, double *states, double *rates, double *variables);
+void computeVariables(double *variables);
