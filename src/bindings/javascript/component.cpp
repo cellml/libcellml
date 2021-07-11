83e427cd4b7b10b75b8aac8e532b2aacaa0fc3f7
hsorby <h.sorby@auckland.ac.nz>
2021-07-11 19:22:30 +1200

    Work on implementing Parser tests for Javascript.

--- src/bindings/javascript/component.cpp
+++ src/bindings/javascript/component.cpp
@@ -1,15 +1,150 @@
+/*
+Copyright libCellML Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 #include <emscripten/bind.h>
 
+#define DAMN_YOU_EMSCRIPTEN
 #include "libcellml/component.h"
+//#include "libcellml/reset.h"
+//#include "libcellml/variable.h"
 
 using namespace emscripten;
 
+//namespace libcellml {
+
+//class ComponentImportedEntity;
+//using ComponentImportedEntityPtr = std::shared_ptr<ComponentImportedEntity>;
+
+//// To work around multiple inheritance we have to create a combined Units
+//// and ImportedEntity class that we can bind with Emscripten.
+//class ComponentImportedEntity: public Component
+//{
+//public:
+//  static ComponentImportedEntityPtr create() noexcept
+//  {
+//    return std::shared_ptr<ComponentImportedEntity> {new ComponentImportedEntity {}};
+//  }
+
+//  static ComponentImportedEntityPtr create(const std::string &name) noexcept
+//  {
+//    return std::shared_ptr<ComponentImportedEntity> {new ComponentImportedEntity {name}};
+//  }
+
+//  // Clone requires special treatment.
+//  ComponentImportedEntityPtr clone()
+//  {
+//    auto c = create();
+
+//    c->setId(id());
+//    c->setName(name());
+//    c->setMath(math());
+
+//    if (isImport()) {
+//      c->setImportSource(importSource());
+//    }
+
+//    c->setImportReference(importReference());
+
+//    for (size_t index = 0; index < variableCount(); ++index) {
+//      auto v = variable(index);
+//      c->addVariable(v->clone());
+//    }
+
+//    for (size_t index = 0; index < resetCount(); ++index) {
+//      auto r = reset(index);
+//      auto rClone = r->clone();
+//      c->addReset(rClone);
+
+//      auto resetVariable = r->variable();
+//      bool found = false;
+//      for (size_t index = 0; (index < variableCount()) && !found; ++index) {
+//        auto v = variable(index);
+//        if (resetVariable == v) {
+//          found = true;
+//          rClone->setVariable(c->variable(index));
+//        }
+//      }
+//      auto testVariable = r->testVariable();
+//      found = false;
+//      for (size_t index = 0; (index < variableCount()) && !found; ++index) {
+//        auto v = variable(index);
+//        if (testVariable == v) {
+//          found = true;
+//          rClone->setTestVariable(c->variable(index));
+//        }
+//      }
+//    }
+
+//    for (size_t index = 0; index < componentCount(); ++index) {
+//      auto cChild = component(index);
+//      c->addComponent(cChild->clone());
+//    }
+
+//    return c;
+//  }
+
+//  // ImportedEntity methods.
+//  bool isImport() const
+//  {
+//    return Component::isImport();
+//  }
+
+//  ImportSourcePtr importSource() const
+//  {
+//    return Component::importSource();
+//  }
+
+//  void setImportSource(const ImportSourcePtr &importSource)
+//  {
+//    Component::setImportSource(importSource);
+//  }
+
+//  std::string importReference() const
+//  {
+//    return Component::importReference();
+//  }
+
+//  void setImportReference(const std::string &reference)
+//  {
+//    return Component::setImportReference(reference);
+//  }
+
+//  bool isResolved() const
+//  {
+//    return Component::isResolved();
+//  }
+
+//private:
+//  ComponentImportedEntity()
+//    : Component()
+//  {
+//  }
+//  explicit ComponentImportedEntity(const std::string &name)
+//    : Component(name)
+//  {
+//  }
+
+//};
+
+//}
 
 EMSCRIPTEN_BINDINGS(libcellml_component) {
 
     class_<libcellml::Component, base<libcellml::ComponentEntity>>("Component")
-        .smart_ptr_constructor("Component", select_overload<libcellml::ComponentPtr()>(&libcellml::Component::create))
+        .smart_ptr<std::shared_ptr<libcellml::Component>>("ComponentPtr")
         .function("setSourceComponent", &libcellml::Component::setSourceComponent)
         .function("appendMath", &libcellml::Component::appendMath)
         .function("math", &libcellml::Component::math)
@@ -32,16 +167,29 @@ EMSCRIPTEN_BINDINGS(libcellml_component) {
         .function("removeResetByIndex", select_overload<bool(size_t)>(&libcellml::Component::removeReset))
         .function("removeResetByReset", select_overload<bool(const libcellml::ResetPtr &)>(&libcellml::Component::removeReset))
         .function("removeAllResets", &libcellml::Component::removeAllResets)
+        .function("requiresImports", &libcellml::Component::requiresImports)
         .function("reset", &libcellml::Component::reset)
         .function("resetCount", &libcellml::Component::resetCount)
         .function("hasReset", &libcellml::Component::hasReset)
         .function("clone", &libcellml::Component::clone)
-        .function("requiresImports", &libcellml::Component::requiresImports)
-        .function("isImport", &libcellml::ImportedEntity::isImport)
-        .function("importSource", &libcellml::ImportedEntity::importSource)
-        .function("setImportSource", &libcellml::ImportedEntity::setImportSource)
-        .function("importReference", &libcellml::ImportedEntity::importReference)
-        .function("setImportReference", &libcellml::ImportedEntity::setImportReference)
-        .function("isResolved", &libcellml::ImportedEntity::isResolved)
+        .function("isImport", &libcellml::Component::isImport)
+        .function("importSource", &libcellml::Component::importSource)
+        .function("setImportSource", &libcellml::Component::setImportSource)
+        .function("importReference", &libcellml::Component::importReference)
+        .function("setImportReference", &libcellml::Component::setImportReference)
+        .function("isResolved", &libcellml::Component::isResolved)
     ;
+
+//    class_<libcellml::ComponentImportedEntity, base<libcellml::Component>>("Component")
+//        .smart_ptr<std::shared_ptr<libcellml::ComponentImportedEntity>>("ComponentPtr")
+//        .constructor(select_overload<libcellml::ComponentImportedEntityPtr()>(&libcellml::ComponentImportedEntity::create))
+//        .constructor(select_overload<libcellml::ComponentImportedEntityPtr(const std::string &)>(&libcellml::ComponentImportedEntity::create))
+//        .function("clone", &libcellml::ComponentImportedEntity::clone)
+//        .function("isImport", &libcellml::ComponentImportedEntity::isImport)
+//        .function("importSource", &libcellml::ComponentImportedEntity::importSource)
+//        .function("setImportSource", &libcellml::ComponentImportedEntity::setImportSource)
+//        .function("importReference", &libcellml::ComponentImportedEntity::importReference)
+//        .function("setImportReference", &libcellml::ComponentImportedEntity::setImportReference)
+//        .function("isResolved", &libcellml::ComponentImportedEntity::isResolved)
+//    ;
 }
--- tests/bindings/javascript/component.test.js
+++ tests/bindings/javascript/component.test.js
@@ -13,3 +13,100 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+
+const libCellMLModule = require('libcellml.js/libcellml.common')
+let libcellml = null
+
+describe("Component tests", () => {
+    beforeAll(async () => {
+        libcellml = await libCellMLModule()
+    })
+    test('Checking Component set source component.', () => {
+        const c = new libcellml.Component()
+        const iS = new libcellml.ImportSource()
+
+        expect(c.requiresImports()).toBe(false)
+
+        c.setSourceComponent(iS, "imported_component_name")
+        expect(c.requiresImports()).toBe(true)
+    })
+    test('Checking Component component count.', () => {
+        const c = new libcellml.Component()
+        const c1 = new libcellml.Component()
+
+        expect(c.componentCount()).toBe(0)
+
+        c.addComponent(c1)
+        expect(c.componentCount()).toBe(1)
+    })
+    test('Checking Component math.', () => {
+        const c = new libcellml.Component()
+
+        expect(c.math()).toBe("")
+
+        c.setMath("some_math")
+        expect(c.math()).toBe("some_math")
+
+        c.appendMath("_and_more_math")
+        expect(c.math()).toBe("some_math_and_more_math")
+
+        c.removeMath()
+        expect(c.math()).toBe("")
+    })
+    test('Checking Component clone.', () => {
+        const c = new libcellml.Component("mine")
+        let iS = new libcellml.ImportSource()
+        iS.setUrl("someplace")
+
+        c.setMath("some_math")
+        c.setImportSource(iS)
+
+        const cClone = c.clone()
+        expect(cClone.name()).toBe("mine")
+        expect(cClone.math()).toBe("some_math")
+        expect(cClone.importSource().url()).toBe("someplace")
+    })
+    test('Checking Component encapsulation id.', () => {
+        const c = new libcellml.Component()
+
+        expect(c.encapsulationId()).toBe("")
+
+        c.setEncapsulationId("bob")
+        expect(c.encapsulationId()).toBe("bob")
+
+        c.removeEncapsulationId()
+        expect(c.encapsulationId()).toBe("")
+    })
+    test('Checking Component import source.', () => {
+        const c = new libcellml.Component()
+        let iS = new libcellml.ImportSource()
+        iS.setUrl("someplace")
+
+        expect(c.isImport()).toBe(false)
+
+        c.setImportSource(iS)
+        expect(c.isImport()).toBe(true)
+        expect(c.importSource().url()).toBe("someplace")
+    })
+    test('Checking Component import reference.', () => {
+        const c = new libcellml.Component()
+
+        expect(c.importReference()).toBe("")
+
+        c.setImportReference("over_here")
+        expect(c.importReference()).toBe("over_here")
+    })
+    test('Checking Units is resolved.', () => {
+        const c = new libcellml.Component()
+
+        expect(c.isResolved()).toBe(true)
+
+        let iS = new libcellml.ImportSource()
+        iS.setUrl("someplace")
+
+        c.setImportSource(iS)
+        c.setImportReference("this_thing")
+        expect(c.isResolved()).toBe(false)
+    })
+})
