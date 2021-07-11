b50cf98700ab25e4f593de5a1e0f0bcd776d2304
hsorby <h.sorby@auckland.ac.nz>
2021-07-11 19:22:30 +1200

    Work on implementing Parser tests for Javascript.

--- src/api/libcellml/component.h
+++ src/api/libcellml/component.h
@@ -426,12 +426,27 @@ public:
      *
      * @return @c true when imports are required, @c false otherwise.
      */
-    bool requiresImports();
+    bool requiresImports() const;
 
-private:
+#ifdef DAMN_YOU_EMSCRIPTEN
+    bool isImport() const;
+
+    ImportSourcePtr importSource() const;
+
+    void setImportSource(const ImportSourcePtr &importSource);
+
+    std::string importReference() const;
+
+    void setImportReference(const std::string &reference);
+
+    bool isResolved() const;
+#endif
+
+protected:
     Component(); /**< Constructor @private*/
     explicit Component(const std::string &name); /**< Constructor named, @private. */
 
+private:
     bool doAddComponent(const ComponentPtr &component) override; /**< Virtual method for implementing addComponent, @private. */
 
     bool doIsResolved() const override; /**< Virtual method for implementing isResolved, @private. */
