42f42648b6203e8e96d8dbe8bb1ab806017c98ae
hsorby <h.sorby@auckland.ac.nz>
2021-09-16 13:02:07 +1200

    Review fixes.

--- tests/api_headers/api_header_test.in.cmake
+++ tests/api_headers/api_header_test.in.cmake
@@ -10,7 +10,7 @@ set_target_properties(${CURRENT_TEST} PROPERTIES
 add_test(NAME api_header_inclusion_${CURRENT_TEST} COMMAND ${CURRENT_TEST})
 
 if(MSVC)
-  # The libxml2 dll and zlib dll must be on the path otherwise the tests will not be able to run.
+  # The libxml2 DLL and zlib DLL must be on the path otherwise the tests will not be able to run.
   set_tests_properties(api_header_inclusion_${CURRENT_TEST}
     PROPERTIES ENVIRONMENT "PATH=$<TARGET_FILE_DIR:cellml>\;$<TARGET_FILE_DIR:gtest_main>${GEN_EXP_XML2_TARGET_FILE_DIR}${GEN_EXP_ZLIB_TARGET_FILE_DIR}")
 endif()
