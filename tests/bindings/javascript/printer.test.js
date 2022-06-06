ed515b1b621c71958ad29aaf4b03df7aff82df8e
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-06-06 15:40:31 +1200

    Add JS binding tests for Printer.

--- tests/bindings/javascript/printer.test.js
+++ tests/bindings/javascript/printer.test.js
@@ -1,4 +1,5 @@
 /*
+
 Copyright libCellML Contributors
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,3 +14,22 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+const loadLibCellML = require('libcellml.js/libcellml.common')
+let libcellml = null
+
+describe("Printer tests", () => {
+    beforeAll(async () => {
+        libcellml = await loadLibCellML()
+    })
+    test('Checking Printer print model.', () => {
+        const p = new libcellml.Printer()
+
+        const model = new libcellml.Model()
+        model.setName("my_model")
+
+        const m = p.printModel(model, false)
+
+        expect(m).toBe("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<model xmlns=\"http://www.cellml.org/cellml/2.0#\" name=\"my_model\"/>\n")
+    })
+})
