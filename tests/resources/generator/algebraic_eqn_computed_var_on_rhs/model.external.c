b235087e87d623cd2644c0d6aee48a6321378cd0
abi-git-user <abi.git.user@gmail.com>
2022-09-14 21:03:07 +1200

    Changing version to: 0.3.103

--- tests/resources/generator/algebraic_eqn_computed_var_on_rhs/model.external.c
+++ tests/resources/generator/algebraic_eqn_computed_var_on_rhs/model.external.c
@@ -1,4 +1,4 @@
-/* The content of this file was generated using the C profile of libCellML 0.3.102. */
+/* The content of this file was generated using the C profile of libCellML 0.3.103. */
 
 #include "model.external.h"
 
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 const char VERSION[] = "0.3.0";
-const char LIBCELLML_VERSION[] = "0.3.102";
+const char LIBCELLML_VERSION[] = "0.3.103";
 
 const size_t VARIABLE_COUNT = 2;
 
