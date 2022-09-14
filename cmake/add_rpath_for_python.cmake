b31230a465fec69b879a13d1981a28bc3077b8b1
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-09-14 12:54:05 +1200

    Add /usr/local/Frameworks as another path to search.

--- cmake/add_rpath_for_python.cmake
+++ cmake/add_rpath_for_python.cmake
@@ -4,6 +4,7 @@ string(REPLACE "${LIST_SEPARATOR}" ";" SHARED_OBJECT_MODULES "${SHARED_OBJECT_MO
 foreach(_M ${SHARED_OBJECT_MODULES})
   execute_process(COMMAND ${INSTALL_NAME_TOOL_EXE} -change "/Library/Frameworks/Python.framework/Versions/${Python_VERSION_MAJOR}.${Python_VERSION_MINOR}/Python" @rpath/Python.framework/Versions/${Python_VERSION_MAJOR}.${Python_VERSION_MINOR}/Python ${_M})
   execute_process(COMMAND ${INSTALL_NAME_TOOL_EXE} -add_rpath /Library/Frameworks ${_M})
+  execute_process(COMMAND ${INSTALL_NAME_TOOL_EXE} -add_rpath /usr/local/Frameworks ${_M})
   execute_process(COMMAND ${INSTALL_NAME_TOOL_EXE} -add_rpath /opt/homebrew/Frameworks ${_M})
 endforeach()
 
