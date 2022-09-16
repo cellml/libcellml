6954743f122b08986b7f874ccdcc11feeebb79ef
abi-git-user <abi.git.user@gmail.com>
2022-09-16 21:38:50 +1200

    Changing version to: 0.3.104

--- tests/resources/generator/hodgkin_huxley_squid_axon_model_1952/model.dependent.state.py
+++ tests/resources/generator/hodgkin_huxley_squid_axon_model_1952/model.dependent.state.py
@@ -1,11 +1,11 @@
-# The content of this file was generated using the Python profile of libCellML 0.3.103.
+# The content of this file was generated using the Python profile of libCellML 0.3.104.
 
 from enum import Enum
 from math import *
 
 
 __version__ = "0.3.0"
-LIBCELLML_VERSION = "0.3.103"
+LIBCELLML_VERSION = "0.3.104"
 
 STATE_COUNT = 2
 VARIABLE_COUNT = 20
