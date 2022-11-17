0407697331ac049860d553d0e08118d9f159abb7
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-11-17 22:46:11 +1300

    Add remove name API to named entity.

--- tests/bindings/python/test_model.py
+++ tests/bindings/python/test_model.py
@@ -15,6 +15,8 @@ class ModelTestCase(unittest.TestCase):
 
         y = Model('bob')
         self.assertEqual('bob', y.name())
+        y.removeName()
+        self.assertEqual('', y.name()
 
         z = libcellml.Model()
         del z
