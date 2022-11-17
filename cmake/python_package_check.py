19c642f68a59e48ce062e5e5e4c79005e593006d
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-11-17 19:50:29 +1300

    Use pip list instead of pip freeze to detect presence of Python package.

--- cmake/python_package_check.py
+++ cmake/python_package_check.py
@@ -1,13 +1,15 @@
 import subprocess
 import sys
 
-reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
-installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'list'])
+
+output_lines = reqs.splitlines()
+# Remove two header lines from pip list output.
+output_lines = output_lines[2:]
+
+installed_packages = [r.decode().split()[0] for r in output_lines]
 
 check_package = sys.argv[1]
-if check_package in installed_packages:
-  result = 0
-else:
-  result = 1
+result = 0 if check_package in installed_packages else 1
 
 sys.exit(result)
