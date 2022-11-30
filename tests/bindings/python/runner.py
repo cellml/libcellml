4c7aa7604919dcd085524ce42224470f6008ca77
hsorby <h.sorby@auckland.ac.nz>
2022-12-01 07:47:09 +1300

    Revert "Adjust python tests to account for SWIG version 4.1.0."
    This reverts commit 6658d42d857f47deca98b5d3b80d32d5d9f0a926.

--- tests/bindings/python/runner.py
+++ tests/bindings/python/runner.py
@@ -15,15 +15,15 @@ arg1 = ""
 if len(args):
     arg1 = args.pop(0)
 
+suite = unittest.TestLoader().discover(".")
+
 cov = coverage.Coverage(source=["libcellml"])
 cov.set_option("run:plugins", ["exclude_until_coverage_plugin"])
 cov.set_option("exclude_until_coverage_plugin:marker", "# libCellML generated wrapper code starts here.")
 
 os.chdir(os.path.join("@PYTHON_COVERAGE_WORKING_DIRECTORY@", configuration))
-
 cov.start()
 
-suite = unittest.TestLoader().discover("@CMAKE_CURRENT_BINARY_DIR@")
 unittest.TextTestRunner(verbosity=1).run(suite)
 
 cov.stop()
