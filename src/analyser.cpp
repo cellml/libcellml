eea36113558b7ea372c8ae5c6d150e7ecb90c7e0
Alan Garny <agarny@hellix.com>
2022-12-21 23:52:11 +1300

    Some minor cleaning up.

--- src/analyser.cpp
+++ src/analyser.cpp
@@ -2567,8 +2567,6 @@ void Analyser::AnalyserImpl::analyseModel(const ModelPtr &model)
 
     // Make our internal equations available through our API.
 
-    std::vector<AnalyserInternalVariablePtr> externallyDependentVariables;
-
     for (const auto &internalEquation : mInternalEquations) {
         // Determine the type of the equation.
 
