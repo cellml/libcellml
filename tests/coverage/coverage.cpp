544810e2e833da7477e047f874c458a8224a7b41
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-02-14 23:15:45 +1300

    Add test to find repeated name in component with math. Find comment deep in MathML.

--- tests/coverage/coverage.cpp
+++ tests/coverage/coverage.cpp
@@ -678,3 +678,47 @@ TEST(Coverage, invalidXmlIds)
     validator->validateModel(model);
     EXPECT_EQ(size_t(1), validator->errorCount());
 }
+
+TEST(Coverage, componentMathWithRepeatedVariableNames)
+{
+    const std::string math =
+        "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">\n"
+        "  <apply>\n"
+        "    <eq/>\n"
+        "    <ci>C</ci>\n"
+        "<!-- valid comment in math -->\n"
+        "    <apply>\n"
+        "      <plus/>\n"
+        "      <ci>A</ci>\n"
+        "      <ci>B</ci>\n"
+        "    </apply>\n"
+        "  </apply>\n"
+        "</math>\n";
+
+    libcellml::ValidatorPtr v = libcellml::Validator::create();
+    libcellml::ModelPtr m = libcellml::Model::create();
+    libcellml::ComponentPtr c = libcellml::Component::create();
+    libcellml::VariablePtr v1 = libcellml::Variable::create();
+    libcellml::VariablePtr v2 = libcellml::Variable::create();
+    libcellml::VariablePtr v3 = libcellml::Variable::create();
+
+    m->setName("modelName");
+    c->setName("componentName");
+    v1->setName("A");
+    v2->setName("B");
+    v3->setName("A");
+    v1->setInitialValue("1.0");
+    v2->setInitialValue("-1.0");
+    v1->setUnits("dimensionless");
+    v2->setUnits("dimensionless");
+    v3->setUnits("dimensionless");
+
+    c->addVariable(v1);
+    c->addVariable(v2);
+    c->addVariable(v3);
+    c->setMath(math);
+    m->addComponent(c);
+
+    v->validateModel(m);
+    EXPECT_EQ(size_t(2), v->issueCount());
+}
