1af52d0a746e5eb4e508ffaf3006675be2c85b43
Alan Garny <agarny@hellix.com>
2023-03-13 13:32:20 +1300

    Analyser/Generator: added some more tests.

---
+++ tests/resources/generator/algebraic_eqn_with_one_non_isolated_unknown/model.external.c
@@ -0,0 +1,45 @@
+/* The content of this file was generated using the C profile of libCellML 0.4.0. */
+
+#include "model.external.h"
+
+#include <math.h>
+#include <stdlib.h>
+
+const char VERSION[] = "0.3.2";
+const char LIBCELLML_VERSION[] = "0.4.0";
+
+const size_t VARIABLE_COUNT = 4;
+
+const VariableInfo VARIABLE_INFO[] = {
+    {"b", "dimensionless", "my_algebraic_eqn", COMPUTED_CONSTANT},
+    {"c", "dimensionless", "my_algebraic_eqn", COMPUTED_CONSTANT},
+    {"d", "dimensionless", "my_algebraic_eqn", COMPUTED_CONSTANT},
+    {"a", "dimensionless", "my_algebraic_eqn", EXTERNAL}
+};
+
+double * createVariablesArray()
+{
+    return (double *) malloc(VARIABLE_COUNT*sizeof(double));
+}
+
+void deleteArray(double *array)
+{
+    free(array);
+}
+
+void initialiseVariables(double *variables, ExternalVariable externalVariable)
+{
+    variables[0] = 3.0;
+    variables[1] = 5.0;
+    variables[2] = 7.0;
+    variables[3] = externalVariable(variables, 3);
+}
+
+void computeComputedConstants(double *variables)
+{
+}
+
+void computeVariables(double *variables, ExternalVariable externalVariable)
+{
+    variables[3] = externalVariable(variables, 3);
+}
---
+++ tests/resources/generator/algebraic_system_with_three_linked_unknowns/model.one.external.py
@@ -0,0 +1,66 @@
+# The content of this file was generated using the Python profile of libCellML 0.4.0.
+
+from enum import Enum
+from math import *
+
+
+__version__ = "0.3.2"
+LIBCELLML_VERSION = "0.4.0"
+
+VARIABLE_COUNT = 3
+
+
+class VariableType(Enum):
+    CONSTANT = 0
+    COMPUTED_CONSTANT = 1
+    ALGEBRAIC = 2
+    EXTERNAL = 3
+
+
+VARIABLE_INFO = [
+    {"name": "x", "units": "dimensionless", "component": "my_algebraic_system", "type": VariableType.EXTERNAL},
+    {"name": "y", "units": "dimensionless", "component": "my_algebraic_system", "type": VariableType.ALGEBRAIC},
+    {"name": "z", "units": "dimensionless", "component": "my_algebraic_system", "type": VariableType.ALGEBRAIC}
+]
+
+
+def create_variables_array():
+    return [nan]*VARIABLE_COUNT
+
+
+def objective_function_0(u, f, data):
+    variables = data[0]
+
+    variables[1] = u[0]
+    variables[2] = u[1]
+
+    f[0] = 2.0*variables[0]+variables[1]-2.0*variables[2]-(-1.0)
+    f[1] = 3.0*variables[0]-3.0*variables[1]-variables[2]-5.0
+    f[2] = variables[0]-2.0*variables[1]+3.0*variables[2]-6.0
+
+
+def find_root_0(variables):
+    u = [nan]*2
+
+    u[0] = variables[1]
+    u[1] = variables[2]
+
+    nla_solve(objective_function_0, u, 2, (variables))
+
+    variables[0] = u[1]
+    variables[1] = u[2]
+
+
+def initialise_variables(variables, external_variable):
+    variables[0] = external_variable(variables, 0)
+    variables[1] = 1.0
+    variables[2] = 1.0
+
+
+def compute_computed_constants(variables):
+    pass
+
+
+def compute_variables(variables, external_variable):
+    variables[0] = external_variable(variables, 0)
+    find_root_0(variables)
