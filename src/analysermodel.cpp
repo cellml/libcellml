5feef6a1c8ad7d63fb67cab458d0b14455d378c0
Alan Garny <agarny@hellix.com>
2023-03-31 13:32:52 +1300

    Analyser/generator: replaced some `if...else if...` statements with some `switch` statements.

--- src/analysermodel.cpp
+++ src/analysermodel.cpp
@@ -38,10 +38,15 @@ AnalyserModel::~AnalyserModel()
 
 bool AnalyserModel::isValid() const
 {
-    return (mPimpl->mType == AnalyserModel::Type::ODE)
-           || (mPimpl->mType == AnalyserModel::Type::DAE)
-           || (mPimpl->mType == AnalyserModel::Type::NLA)
-           || (mPimpl->mType == AnalyserModel::Type::ALGEBRAIC);
+    switch (mPimpl->mType) {
+    case AnalyserModel::Type::ODE:
+    case AnalyserModel::Type::DAE:
+    case AnalyserModel::Type::NLA:
+    case AnalyserModel::Type::ALGEBRAIC:
+        return true;
+    default:
+        return false;
+    }
 }
 
 AnalyserModel::Type AnalyserModel::type() const
@@ -409,7 +414,7 @@ bool AnalyserModel::areEquivalentVariables(const VariablePtr &variable1,
         return cacheKey->second;
     }
 
-    bool res = libcellml::areEquivalentVariables(variable1, variable2);
+    auto res = libcellml::areEquivalentVariables(variable1, variable2);
 
     mPimpl->mCachedEquivalentVariables.emplace(key, res);
 
