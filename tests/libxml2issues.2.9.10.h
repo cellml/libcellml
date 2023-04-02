4938d320b1d280a09e6b860eb093b480627215c8
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-04-02 23:04:43 +1200

    Fix expected error messages in tests/libxml2issues.2.9.10.h.

--- tests/libxml2issues.2.9.10.h
+++ tests/libxml2issues.2.9.10.h
@@ -7,7 +7,6 @@ const std::vector<std::string> expectedLibXml2Issues = {
     "LibXml2 error: Opening and ending tag mismatch: ci line 6 and apply.",
     "LibXml2 error: Opening and ending tag mismatch: ci line 6 and math.",
     "LibXml2 error: Opening and ending tag mismatch: apply line 6 and math_wrap_as_single_root_element.",
-    "LibXml2 error: Premature end of data in tag math line 2",
-    "LibXml2 error: Premature end of data in tag math_wrap_as_single_root_element line 1.",
+    "LibXml2 error: EndTag: '</' not found.",
 };
 
