2d8e4efbddc00800dc24caebbaa8ac5874bf7b79
Alan Garny <agarny@hellix.com>
2023-04-06 01:47:18 +1200

    Fixed merging issue.

--- tests/resources/generator/variable_initialised_using_a_constant/model.c
+++ tests/resources/generator/variable_initialised_using_a_constant/model.c
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-const char VERSION[] = "0.3.1";
+const char VERSION[] = "0.3.2";
 const char LIBCELLML_VERSION[] = "0.4.0";
 
 const size_t STATE_COUNT = 2;
@@ -19,21 +19,21 @@ const VariableInfo STATE_INFO[] = {
 };
 
 const VariableInfo VARIABLE_INFO[] = {
-    {"F", "C_per_mol", "BG_initTest_param", CONSTANT},
-    {"K_Ai", "per_fmol", "BG_initTest_param", CONSTANT},
-    {"K_Ao", "per_fmol", "BG_initTest_param", CONSTANT},
-    {"R", "J_per_K_per_mol", "BG_initTest_param", CONSTANT},
-    {"T", "kelvin", "BG_initTest_param", CONSTANT},
-    {"kappa_Re1", "fmol_per_sec", "BG_initTest_param", CONSTANT},
-    {"q_Ai_init", "fmol", "BG_initTest_param", CONSTANT},
-    {"q_Ao_init", "fmol", "BG_initTest_param", CONSTANT},
     {"mu_Ai", "J_per_mol", "BG_initTest", ALGEBRAIC},
+    {"R", "J_per_K_per_mol", "BG_initTest_param", CONSTANT},
+    {"K_Ai", "per_fmol", "BG_initTest_param", CONSTANT},
+    {"T", "kelvin", "BG_initTest_param", CONSTANT},
+    {"v_Ai", "fmol_per_sec", "BG_initTest", ALGEBRAIC},
     {"mu_Ao", "J_per_mol", "BG_initTest", ALGEBRAIC},
+    {"K_Ao", "per_fmol", "BG_initTest_param", CONSTANT},
+    {"v_Ao", "fmol_per_sec", "BG_initTest", ALGEBRAIC},
+    {"v_Re1", "fmol_per_sec", "BG_initTest", ALGEBRAIC},
+    {"kappa_Re1", "fmol_per_sec", "BG_initTest_param", CONSTANT},
     {"mu_Re1_in", "J_per_mol", "BG_initTest", ALGEBRAIC},
     {"mu_Re1_out", "J_per_mol", "BG_initTest", ALGEBRAIC},
-    {"v_Re1", "fmol_per_sec", "BG_initTest", ALGEBRAIC},
-    {"v_Ai", "fmol_per_sec", "BG_initTest", ALGEBRAIC},
-    {"v_Ao", "fmol_per_sec", "BG_initTest", ALGEBRAIC}
+    {"q_Ai_init", "fmol", "BG_initTest_param", CONSTANT},
+    {"q_Ao_init", "fmol", "BG_initTest_param", CONSTANT},
+    {"F", "C_per_mol", "BG_initTest_param", CONSTANT}
 };
 
 double * createStatesArray()
@@ -53,16 +53,16 @@ void deleteArray(double *array)
 
 void initialiseVariables(double *states, double *variables)
 {
-    variables[0] = 96485.0;
-    variables[1] = 1.0;
+    variables[1] = 8.31;
     variables[2] = 1.0;
-    variables[3] = 8.31;
-    variables[4] = 293.0;
-    variables[5] = 1.0;
+    variables[3] = 293.0;
     variables[6] = 1.0;
-    variables[7] = 1.0;
-    states[0] = variables[6];
-    states[1] = variables[7];
+    variables[9] = 1.0;
+    variables[12] = 1.0;
+    variables[13] = 1.0;
+    variables[14] = 96485.0;
+    states[0] = variables[12];
+    states[1] = variables[13];
 }
 
 void computeComputedConstants(double *variables)
@@ -71,24 +71,24 @@ void computeComputedConstants(double *variables)
 
 void computeRates(double voi, double *states, double *rates, double *variables)
 {
-    variables[9] = variables[3]*variables[4]*log(variables[2]*states[1]);
-    variables[10] = variables[9];
-    variables[8] = variables[3]*variables[4]*log(variables[1]*states[0]);
-    variables[11] = variables[8];
-    variables[12] = variables[5]*(exp(variables[10]/(variables[3]*variables[4]))-exp(variables[11]/(variables[3]*variables[4])));
-    variables[13] = variables[12];
-    rates[0] = variables[13];
-    variables[14] = -variables[12];
-    rates[1] = variables[14];
+    variables[5] = variables[1]*variables[3]*log(variables[6]*states[1]);
+    variables[10] = variables[5];
+    variables[0] = variables[1]*variables[3]*log(variables[2]*states[0]);
+    variables[11] = variables[0];
+    variables[8] = variables[9]*(exp(variables[10]/(variables[1]*variables[3]))-exp(variables[11]/(variables[1]*variables[3])));
+    variables[4] = variables[8];
+    rates[0] = variables[4];
+    variables[7] = -variables[8];
+    rates[1] = variables[7];
 }
 
 void computeVariables(double voi, double *states, double *rates, double *variables)
 {
-    variables[8] = variables[3]*variables[4]*log(variables[1]*states[0]);
-    variables[9] = variables[3]*variables[4]*log(variables[2]*states[1]);
-    variables[10] = variables[9];
-    variables[11] = variables[8];
-    variables[12] = variables[5]*(exp(variables[10]/(variables[3]*variables[4]))-exp(variables[11]/(variables[3]*variables[4])));
-    variables[13] = variables[12];
-    variables[14] = -variables[12];
+    variables[0] = variables[1]*variables[3]*log(variables[2]*states[0]);
+    variables[5] = variables[1]*variables[3]*log(variables[6]*states[1]);
+    variables[10] = variables[5];
+    variables[11] = variables[0];
+    variables[8] = variables[9]*(exp(variables[10]/(variables[1]*variables[3]))-exp(variables[11]/(variables[1]*variables[3])));
+    variables[4] = variables[8];
+    variables[7] = -variables[8];
 }
--- tests/resources/generator/variable_initialised_using_a_constant/model.py
+++ tests/resources/generator/variable_initialised_using_a_constant/model.py
@@ -4,7 +4,7 @@ from enum import Enum
 from math import *
 
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 LIBCELLML_VERSION = "0.4.0"
 
 STATE_COUNT = 2
@@ -27,21 +27,21 @@ STATE_INFO = [
 ]
 
 VARIABLE_INFO = [
-    {"name": "F", "units": "C_per_mol", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
-    {"name": "K_Ai", "units": "per_fmol", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
-    {"name": "K_Ao", "units": "per_fmol", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
-    {"name": "R", "units": "J_per_K_per_mol", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
-    {"name": "T", "units": "kelvin", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
-    {"name": "kappa_Re1", "units": "fmol_per_sec", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
-    {"name": "q_Ai_init", "units": "fmol", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
-    {"name": "q_Ao_init", "units": "fmol", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
     {"name": "mu_Ai", "units": "J_per_mol", "component": "BG_initTest", "type": VariableType.ALGEBRAIC},
+    {"name": "R", "units": "J_per_K_per_mol", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
+    {"name": "K_Ai", "units": "per_fmol", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
+    {"name": "T", "units": "kelvin", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
+    {"name": "v_Ai", "units": "fmol_per_sec", "component": "BG_initTest", "type": VariableType.ALGEBRAIC},
     {"name": "mu_Ao", "units": "J_per_mol", "component": "BG_initTest", "type": VariableType.ALGEBRAIC},
+    {"name": "K_Ao", "units": "per_fmol", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
+    {"name": "v_Ao", "units": "fmol_per_sec", "component": "BG_initTest", "type": VariableType.ALGEBRAIC},
+    {"name": "v_Re1", "units": "fmol_per_sec", "component": "BG_initTest", "type": VariableType.ALGEBRAIC},
+    {"name": "kappa_Re1", "units": "fmol_per_sec", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
     {"name": "mu_Re1_in", "units": "J_per_mol", "component": "BG_initTest", "type": VariableType.ALGEBRAIC},
     {"name": "mu_Re1_out", "units": "J_per_mol", "component": "BG_initTest", "type": VariableType.ALGEBRAIC},
-    {"name": "v_Re1", "units": "fmol_per_sec", "component": "BG_initTest", "type": VariableType.ALGEBRAIC},
-    {"name": "v_Ai", "units": "fmol_per_sec", "component": "BG_initTest", "type": VariableType.ALGEBRAIC},
-    {"name": "v_Ao", "units": "fmol_per_sec", "component": "BG_initTest", "type": VariableType.ALGEBRAIC}
+    {"name": "q_Ai_init", "units": "fmol", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
+    {"name": "q_Ao_init", "units": "fmol", "component": "BG_initTest_param", "type": VariableType.CONSTANT},
+    {"name": "F", "units": "C_per_mol", "component": "BG_initTest_param", "type": VariableType.CONSTANT}
 ]
 
 
@@ -54,16 +54,16 @@ def create_variables_array():
 
 
 def initialise_variables(states, variables):
-    variables[0] = 96485.0
-    variables[1] = 1.0
+    variables[1] = 8.31
     variables[2] = 1.0
-    variables[3] = 8.31
-    variables[4] = 293.0
-    variables[5] = 1.0
+    variables[3] = 293.0
     variables[6] = 1.0
-    variables[7] = 1.0
-    states[0] = variables[6]
-    states[1] = variables[7]
+    variables[9] = 1.0
+    variables[12] = 1.0
+    variables[13] = 1.0
+    variables[14] = 96485.0
+    states[0] = variables[12]
+    states[1] = variables[13]
 
 
 def compute_computed_constants(variables):
@@ -71,22 +71,22 @@ def compute_computed_constants(variables):
 
 
 def compute_rates(voi, states, rates, variables):
-    variables[9] = variables[3]*variables[4]*log(variables[2]*states[1])
-    variables[10] = variables[9]
-    variables[8] = variables[3]*variables[4]*log(variables[1]*states[0])
-    variables[11] = variables[8]
-    variables[12] = variables[5]*(exp(variables[10]/(variables[3]*variables[4]))-exp(variables[11]/(variables[3]*variables[4])))
-    variables[13] = variables[12]
-    rates[0] = variables[13]
-    variables[14] = -variables[12]
-    rates[1] = variables[14]
+    variables[5] = variables[1]*variables[3]*log(variables[6]*states[1])
+    variables[10] = variables[5]
+    variables[0] = variables[1]*variables[3]*log(variables[2]*states[0])
+    variables[11] = variables[0]
+    variables[8] = variables[9]*(exp(variables[10]/(variables[1]*variables[3]))-exp(variables[11]/(variables[1]*variables[3])))
+    variables[4] = variables[8]
+    rates[0] = variables[4]
+    variables[7] = -variables[8]
+    rates[1] = variables[7]
 
 
 def compute_variables(voi, states, rates, variables):
-    variables[8] = variables[3]*variables[4]*log(variables[1]*states[0])
-    variables[9] = variables[3]*variables[4]*log(variables[2]*states[1])
-    variables[10] = variables[9]
-    variables[11] = variables[8]
-    variables[12] = variables[5]*(exp(variables[10]/(variables[3]*variables[4]))-exp(variables[11]/(variables[3]*variables[4])))
-    variables[13] = variables[12]
-    variables[14] = -variables[12]
+    variables[0] = variables[1]*variables[3]*log(variables[2]*states[0])
+    variables[5] = variables[1]*variables[3]*log(variables[6]*states[1])
+    variables[10] = variables[5]
+    variables[11] = variables[0]
+    variables[8] = variables[9]*(exp(variables[10]/(variables[1]*variables[3]))-exp(variables[11]/(variables[1]*variables[3])))
+    variables[4] = variables[8]
+    variables[7] = -variables[8]
