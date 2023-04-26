a92ed07c723445edfc1684e8bb14d9a465e6727b
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-04-25 22:11:43 -0700

    Missed parentheses correction.

--- src/annotator.cpp
+++ src/annotator.cpp
@@ -879,7 +879,9 @@ bool Annotator::assignIds(CellmlElementType type)
 bool assignEncapsulationId(const ComponentPtr &component, CellmlElementType type, bool all)
 {
     bool inHierarchy = std::dynamic_pointer_cast<libcellml::Model>(component->parent()) == nullptr || component->componentCount() > 0;
-    return (type == CellmlElementType::COMPONENT_REF || all) && component->encapsulationId().empty() && inHierarchy;
+    return ((type == CellmlElementType::COMPONENT_REF) || all)
+        && component->encapsulationId().empty()
+        && inHierarchy;
 }
 
 void Annotator::AnnotatorImpl::doSetComponentTreeTypeIds(const ComponentPtr &component, CellmlElementType type, bool all)
