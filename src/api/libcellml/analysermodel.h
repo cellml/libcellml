78d4a69b0b8ea658fda4917ed3151f917722c1ac
Alan Garny <agarny@hellix.com>
2023-04-26 19:00:15 -0700

    Updated the documentation for AnalyserModel::Type.

--- src/api/libcellml/analysermodel.h
+++ src/api/libcellml/analysermodel.h
@@ -37,18 +37,19 @@ public:
      * The type of a model, i.e. whether it is unknown, invalid,
      * underconstrained, overconstrained, or unsuitably constrained, or whether
      * it defines a system of algebraic equations, a system of non-linear
-     * algebraic equations, or a system of ordinary differential equations.
+     * algebraic equations, a system of ordinary differential equations, or
+     * a system of differential algebraic equations.
      */
     enum class Type
     {
         UNKNOWN, /**< The type of the model is unknown. */
-        ODE, /**< The model defines a system of ordinary differential equations that require an external ODE solver (e.g., CVODE) to solve. */
-        DAE, /**< The model defines a system of differential algebraic equations that require both an external ODE solver (e.g., CVODE) and an external NLA solver (e.g., KINSOL) to solve. */
-        NLA, /**< The model defines a system of (potentially non-linear) algebraic equations that require an external NLA solver (e.g., KINSOL) to solve. */
         ALGEBRAIC, /**< The model defines a system of algebraic equations that can be solved directly. */
+        DAE, /**< The model defines a system of differential algebraic equations that require both an external ODE solver (e.g., CVODE) and an external NLA solver (e.g., KINSOL) to solve. */
         INVALID, /**< The model is invalid. */
-        UNDERCONSTRAINED, /**< The model is underconstrainted. */
+        NLA, /**< The model defines a system of (potentially non-linear) algebraic equations that require an external NLA solver (e.g., KINSOL) to solve. */
+        ODE, /**< The model defines a system of ordinary differential equations that require an external ODE solver (e.g., CVODE) to solve. */
         OVERCONSTRAINED, /**< The model is overconstrained. */
+        UNDERCONSTRAINED, /**< The model is underconstrainted. */
         UNSUITABLY_CONSTRAINED /**< The model is unsuitably constrained. */
     };
 
