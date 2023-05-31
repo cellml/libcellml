3d6e0ca9a81232a57d4a5fd1bc0a48c89b4039a8
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-05-31 16:38:56 +1200

    Correct address of Component isDefined function for Javaascript bindings.

--- src/bindings/javascript/component.cpp
+++ src/bindings/javascript/component.cpp
@@ -63,7 +63,7 @@ EMSCRIPTEN_BINDINGS(libcellml_component) {
         .function("importReference", &libcellml::Component::importReference)
         .function("setImportReference", &libcellml::Component::setImportReference)
         .function("isResolved", &libcellml::Component::isResolved)
-        .function("isDefined", &libcellml::Units::isDefined)
+        .function("isDefined", &libcellml::Component::isDefined)
     ;
 
 }
