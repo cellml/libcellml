3b4e2d4775fc12d3253ec64ad378114cbfccebfb
abi-git-user <abi.git.user@gmail.com>
2023-10-10 11:57:58 +1300

    Changing version to: 0.5.0-rc.1

--- tests/bindings/javascript/version.test.js
+++ tests/bindings/javascript/version.test.js
@@ -22,6 +22,6 @@ describe("Version tests", () => {
     libcellml = await libCellMLModule();
   });
   test('Checking version string.', () => {
-    expect(libcellml.versionString()).toBe('0.4.0');
+    expect(libcellml.versionString()).toBe('0.5.0');
   });
 })
