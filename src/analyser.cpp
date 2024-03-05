de00c1c0e8e085b100868b1f642c499dfb28df3e
Alan Garny <agarny@hellix.com>
2024-03-06 12:57:11 +1300

    Windows: make MSVC happy.
    So that it can use `M_PI`.

--- src/analyser.cpp
+++ src/analyser.cpp
@@ -14,6 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#ifdef _WIN32
+#    define _USE_MATH_DEFINES
+#endif
+
 #include "libcellml/analyser.h"
 
 #include <cmath>
