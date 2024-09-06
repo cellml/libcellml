49b3a4fdde1dd478b9f1c266e5322850bcaba28e
Alan Garny <agarny@hellix.com>
2024-09-06 13:24:12 +1200

    Tests: changed the external variable in algebraicEqnWithOneNonIsolatedUnknownWithExternalVariable.
    This is so that we can test an algebraic model with an external variable (in https://github.com/agarny/libCellMLGeneratedCodeTests).

--- tests/resources/generator/algebraic_eqn_with_one_non_isolated_unknown/model.external.c
+++ tests/resources/generator/algebraic_eqn_with_one_non_isolated_unknown/model.external.c
@@ -9,8 +9,8 @@ const char VERSION[] = "0.6.0";
 const char LIBCELLML_VERSION[] = "0.5.0";
 
 const size_t CONSTANT_COUNT = 0;
-const size_t COMPUTED_CONSTANT_COUNT = 3;
-const size_t ALGEBRAIC_COUNT = 0;
+const size_t COMPUTED_CONSTANT_COUNT = 2;
+const size_t ALGEBRAIC_COUNT = 1;
 const size_t EXTERNAL_COUNT = 1;
 
 const VariableInfo CONSTANT_INFO[] = {
@@ -18,15 +18,15 @@ const VariableInfo CONSTANT_INFO[] = {
 
 const VariableInfo COMPUTED_CONSTANT_INFO[] = {
     {"b", "dimensionless", "my_algebraic_eqn"},
-    {"c", "dimensionless", "my_algebraic_eqn"},
     {"d", "dimensionless", "my_algebraic_eqn"}
 };
 
 const VariableInfo ALGEBRAIC_INFO[] = {
+    {"a", "dimensionless", "my_algebraic_eqn"}
 };
 
 const VariableInfo EXTERNAL_INFO[] = {
-    {"a", "dimensionless", "my_algebraic_eqn"}
+    {"c", "dimensionless", "my_algebraic_eqn"}
 };
 
 double * createConstantsArray()
@@ -78,11 +78,45 @@ void deleteArray(double *array)
     free(array);
 }
 
+typedef struct {
+    double *constants;
+    double *computedConstants;
+    double *algebraic;
+    double *externals;
+} RootFindingInfo;
+
+extern void nlaSolve(void (*objectiveFunction)(double *, double *, void *),
+                     double *u, size_t n, void *data);
+
+void objectiveFunction0(double *u, double *f, void *data)
+{
+    double *constants = ((RootFindingInfo *) data)->constants;
+    double *computedConstants = ((RootFindingInfo *) data)->computedConstants;
+    double *algebraic = ((RootFindingInfo *) data)->algebraic;
+    double *externals = ((RootFindingInfo *) data)->externals;
+
+    algebraic[0] = u[0];
+
+    f[0] = algebraic[0]+computedConstants[0]-(externals[0]+computedConstants[1]);
+}
+
+void findRoot0(double *constants, double *computedConstants, double *algebraic, double *externals)
+{
+    RootFindingInfo rfi = { constants, computedConstants, algebraic, externals };
+    double u[1];
+
+    u[0] = algebraic[0];
+
+    nlaSolve(objectiveFunction0, u, 1, &rfi);
+
+    algebraic[0] = u[0];
+}
+
 void initialiseVariables(double *constants, double *computedConstants, double *algebraic)
 {
     computedConstants[0] = 3.0;
-    computedConstants[1] = 5.0;
-    computedConstants[2] = 7.0;
+    computedConstants[1] = 7.0;
+    algebraic[0] = 1.0;
 }
 
 void computeComputedConstants(double *constants, double *computedConstants)
@@ -92,4 +126,5 @@ void computeComputedConstants(double *constants, double *computedConstants)
 void computeVariables(double *constants, double *computedConstants, double *algebraic, double *externals, ExternalVariable externalVariable)
 {
     externals[0] = externalVariable(constants, computedConstants, algebraic, externals, 0);
+    findRoot0(constants, computedConstants, algebraic, externals);
 }
