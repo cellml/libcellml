e159174fa8ee67a90404e02d6550e188e193a743
abi-git-user <abi.git.user@gmail.com>
2024-11-11 15:26:19 +1300

    Changing version to: 0.6.3-rc.1

--- tests/bindings/javascript/version.test.js
+++ tests/bindings/javascript/version.test.js
@@ -22,6 +22,6 @@ describe("Version tests", () => {
     libcellml = await libCellMLModule();
   });
   test('Checking version string.', () => {
-    expect(libcellml.versionString()).toBe('0.6.2');
+    expect(libcellml.versionString()).toBe('0.6.3');
   });
 })
--- tests/version/version.cpp
+++ tests/version/version.cpp
@@ -5,8 +5,8 @@
 TEST(Version, versionMatch)
 {
     unsigned int version = libcellml::version();
-    EXPECT_EQ(0x000602U, version);
+    EXPECT_EQ(0x000603U, version);
 
     std::string versionString = libcellml::versionString();
-    EXPECT_EQ("0.6.2", versionString);
+    EXPECT_EQ("0.6.3", versionString);
 }
