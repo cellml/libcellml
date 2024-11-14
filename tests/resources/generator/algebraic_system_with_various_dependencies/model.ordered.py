e6583cff527166fc7cd793572bac29eff6e5ce8d
Alan Garny <agarny@hellix.com>
2024-11-15 09:32:52 +1300

    Reverted some "cleaning up" of the analyser.
    Effectively reverting 4357d2b84. Indeed, the code is less efficient: there are more `if` statements and we make many calls (~94% of the time) to AnalyserEquationAst::swapLeftAndRightChildren(). Also, if we were to do that cleaning up then we also need to clean in the case of a piecewise statement.

--- tests/resources/generator/algebraic_system_with_various_dependencies/model.ordered.py
+++ tests/resources/generator/algebraic_system_with_various_dependencies/model.ordered.py
@@ -17,9 +17,9 @@ class VariableType(Enum):
 
 
 VARIABLE_INFO = [
-    {"name": "y", "units": "dimensionless", "component": "my_algebraic_system", "type": VariableType.CONSTANT},
-    {"name": "x", "units": "dimensionless", "component": "my_algebraic_system", "type": VariableType.CONSTANT},
     {"name": "a", "units": "dimensionless", "component": "my_algebraic_system", "type": VariableType.COMPUTED_CONSTANT},
+    {"name": "x", "units": "dimensionless", "component": "my_algebraic_system", "type": VariableType.CONSTANT},
+    {"name": "y", "units": "dimensionless", "component": "my_algebraic_system", "type": VariableType.CONSTANT},
     {"name": "c", "units": "dimensionless", "component": "my_algebraic_system", "type": VariableType.ALGEBRAIC},
     {"name": "b", "units": "dimensionless", "component": "my_algebraic_system", "type": VariableType.ALGEBRAIC},
     {"name": "d", "units": "dimensionless", "component": "my_algebraic_system", "type": VariableType.ALGEBRAIC}
@@ -39,8 +39,8 @@ def objective_function_0(u, f, data):
     variables[3] = u[0]
     variables[4] = u[1]
 
-    f[0] = 3.0*variables[2]+2.0*variables[4]+variables[3]-57.0
-    f[1] = variables[2]+3.0*variables[4]-variables[3]-19.0
+    f[0] = 3.0*variables[0]+2.0*variables[4]+variables[3]-57.0
+    f[1] = variables[0]+3.0*variables[4]-variables[3]-19.0
 
 
 def find_root_0(variables):
@@ -56,14 +56,14 @@ def find_root_0(variables):
 
 
 def initialise_variables(variables):
-    variables[0] = 5.0
     variables[1] = 3.0
+    variables[2] = 5.0
     variables[3] = 1.0
     variables[4] = 1.0
 
 
 def compute_computed_constants(variables):
-    variables[2] = 3.0*variables[1]+variables[0]
+    variables[0] = 3.0*variables[1]+variables[2]
 
 
 def compute_variables(variables):
