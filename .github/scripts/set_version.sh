ea0d75b2727c57e6df438255557d2cd73429235a
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-04-28 21:53:52 +1200

    Change find regexp in release-prepare.yml.

--- .github/scripts/set_version.sh
+++ .github/scripts/set_version.sh
@@ -12,21 +12,21 @@ sed -i 's@    EXPECT_EQ(0x[[:digit:]]+U, version);@    EXPECT_EQ(0x'${numeric_ve
 sed -i 's/^set(_PROJECT_VERSION[^)]*)$/set(_PROJECT_VERSION '${version}')/' CMakeLists.txt
 sed -i 's/^set(PROJECT_DEVELOPER_VERSION[^)]*)$/set(PROJECT_DEVELOPER_VERSION '${developer_version}')/' CMakeLists.txt
 
-files=`grep -rl "^LIBCELLML_VERSION = \"\d+.\d+.\d+\"" *`
+files=`grep -rl "^LIBCELLML_VERSION = \"\d*\.\d*\.\d*\"" *`
 echo "files 0: $files"
 sed -i 's@LIBCELLML_VERSION = \"[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\"@LIBCELLML_VERSION = \"'${version}'\"@' $files
 
-files=`grep -rl "/\* The content of this file was generated using \(the\|a modified\) C profile of libCellML \d+\.\d+\.\d+\. \*/" *`
+files=`grep -rl "/\* The content of this file was generated using \(the\|a modified\) C profile of libCellML \d*\.\d*\.\d*\. \*/" *`
 echo "files 1: $files"
 sed -i -E 's@/\* The content of this file was generated using (the|a modified) C profile of libCellML [[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\. \*/@/\* The content of this file was generated using \1 C profile of libCellML '${version}'. \*/@' $files
 
-files=`grep -rl --exclude=simple-sed.sh "# The content of this file was generated using \(the\|a modified\) Python profile of libCellML \d+\.\d+\.\d+\." *`
+files=`grep -rl --exclude=simple-sed.sh "# The content of this file was generated using \(the\|a modified\) Python profile of libCellML \d*\.\d*\.\d*\." *`
 sed -i -E 's@# The content of this file was generated using (the|a modified) Python profile of libCellML [[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.@# The content of this file was generated using \1 Python profile of libCellML '${version}'.@' $files
 
-files=`grep -rl --exclude=simple-sed.sh "const char LIBCELLML_VERSION\[\] = \"\d+\.\d+\.\d+\";" *`
+files=`grep -rl --exclude=simple-sed.sh "const char LIBCELLML_VERSION\[\] = \"\d*\.\d*\.\d*\";" *`
 sed -i 's@const char LIBCELLML_VERSION\[\] = \"[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\";@const char LIBCELLML_VERSION\[\] = \"'${version}'\";@' $files
 
-files=`grep -rl --exclude=simple-sed.sh "    expect(libcellml\.versionString()).toBe('\d+\.\d+\.\d+');" *`
+files=`grep -rl --exclude=simple-sed.sh "    expect(libcellml\.versionString()).toBe('\d*\.\d*\.\d*');" *`
 sed -i "s@    expect(libcellml\.versionString()).toBe('[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+');@    expect(libcellml\.versionString()).toBe('"${version}"');@" $files
 
 git status
