cf1c6a85be84229e926e7c78b6e6308a12ddbb86
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-04-29 14:34:03 +1200

    Debug 4.

--- .github/scripts/set_version.sh
+++ .github/scripts/set_version.sh
@@ -8,24 +8,16 @@ numeric_version=$(printf %02d "${version_array[@]}")
 version_regex='[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*'
 
 sed -i "s@    EXPECT_EQ(\"${version_regex}\", versionString);@    EXPECT_EQ(\"${version}\", versionString);@" tests/version/version.cpp
-echo "1"
-ls
-git status
 sed -i 's@    EXPECT_EQ(0x[[:digit:]]+U, version);@    EXPECT_EQ(0x'${numeric_version}'U, version);@' tests/version/version.cpp
-echo "2"
-git status
 
 sed -i 's/^set(_PROJECT_VERSION[^)]*)$/set(_PROJECT_VERSION '${version}')/' CMakeLists.txt
 sed -i 's/^set(PROJECT_DEVELOPER_VERSION[^)]*)$/set(PROJECT_DEVELOPER_VERSION '${developer_version}')/' CMakeLists.txt
-git status
+
 
 files=$(grep -rl "^LIBCELLML_VERSION = \"${version_regex}\"" .)
-echo "files 0:"
-echo $files
 sed -i "s@LIBCELLML_VERSION = \"${version_regex}\"@LIBCELLML_VERSION = \"${version}\"@" $files
 
 files=$(grep -rl "/\* The content of this file was generated using \(the\|a modified\) C profile of libCellML ${version_regex}\. \*/" *)
-echo "files 1: $files"
 sed -i -E "s@/\* The content of this file was generated using (the|a modified) C profile of libCellML ${version_regex}\. \*/@/\* The content of this file was generated using \1 C profile of libCellML ${version}. \*/@" $files
 
 files=$(grep -rl --exclude=simple-sed.sh "# The content of this file was generated using \(the\|a modified\) Python profile of libCellML ${version_regex}\." *)
@@ -37,8 +29,4 @@ sed -i "s@const char LIBCELLML_VERSION\[\] = \"${version_regex}\";@const char LI
 files=$(grep -rl --exclude=simple-sed.sh "    expect(libcellml\.versionString()).toBe('${version_regex}');" *)
 sed -i "s@    expect(libcellml\.versionString()).toBe('${version_regex}');@    expect(libcellml\.versionString()).toBe('${version}');@" $files
 
-git status
-echo "==================="
-git diff | cat
-echo "==================="
 exit 0
