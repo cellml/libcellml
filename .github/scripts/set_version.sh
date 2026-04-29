2ee9ee41e3028d4975df3ddc193e28677c33a1ae
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-04-29 14:28:43 +1200

    Debug 3.

--- .github/scripts/set_version.sh
+++ .github/scripts/set_version.sh
@@ -4,9 +4,10 @@ version=$1
 developer_version=$2
 
 IFS='.' read -ra version_array <<< "$version"
-numeric_version=`printf %02d "${version_array[@]}"`
+numeric_version=$(printf %02d "${version_array[@]}")
+version_regex='[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*'
 
-sed -i 's@    EXPECT_EQ(\"[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*\", versionString);@    EXPECT_EQ(\"'${version}'\", versionString);@' tests/version/version.cpp
+sed -i "s@    EXPECT_EQ(\"${version_regex}\", versionString);@    EXPECT_EQ(\"${version}\", versionString);@" tests/version/version.cpp
 echo "1"
 ls
 git status
@@ -18,22 +19,23 @@ sed -i 's/^set(_PROJECT_VERSION[^)]*)$/set(_PROJECT_VERSION '${version}')/' CMak
 sed -i 's/^set(PROJECT_DEVELOPER_VERSION[^)]*)$/set(PROJECT_DEVELOPER_VERSION '${developer_version}')/' CMakeLists.txt
 git status
 
-files=$(grep -rl '^LIBCELLML_VERSION = "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+"' .)
-echo "files 0: $files"
-sed -i 's@LIBCELLML_VERSION = \"[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\"@LIBCELLML_VERSION = \"'${version}'\"@' $files
+files=$(grep -rl "^LIBCELLML_VERSION = \"${version_regex}\"" .)
+echo "files 0:"
+echo $files
+sed -i "s@LIBCELLML_VERSION = \"${version_regex}\"@LIBCELLML_VERSION = \"${version}\"@" $files
 
-files=`grep -rl "/\* The content of this file was generated using \(the\|a modified\) C profile of libCellML \d*\.\d*\.\d*\. \*/" *`
+files=$(grep -rl "/\* The content of this file was generated using \(the\|a modified\) C profile of libCellML ${version_regex}\. \*/" *)
 echo "files 1: $files"
-sed -i -E 's@/\* The content of this file was generated using (the|a modified) C profile of libCellML [[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\. \*/@/\* The content of this file was generated using \1 C profile of libCellML '${version}'. \*/@' $files
+sed -i -E "s@/\* The content of this file was generated using (the|a modified) C profile of libCellML ${version_regex}\. \*/@/\* The content of this file was generated using \1 C profile of libCellML ${version}. \*/@" $files
 
-files=`grep -rl --exclude=simple-sed.sh "# The content of this file was generated using \(the\|a modified\) Python profile of libCellML \d*\.\d*\.\d*\." *`
-sed -i -E 's@# The content of this file was generated using (the|a modified) Python profile of libCellML [[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.@# The content of this file was generated using \1 Python profile of libCellML '${version}'.@' $files
+files=$(grep -rl --exclude=simple-sed.sh "# The content of this file was generated using \(the\|a modified\) Python profile of libCellML ${version_regex}\." *)
+sed -i -E "s@# The content of this file was generated using (the|a modified) Python profile of libCellML ${version_regex}\.@# The content of this file was generated using \1 Python profile of libCellML ${version}.@" $files
 
-files=`grep -rl --exclude=simple-sed.sh "const char LIBCELLML_VERSION\[\] = \"\d*\.\d*\.\d*\";" *`
-sed -i 's@const char LIBCELLML_VERSION\[\] = \"[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\";@const char LIBCELLML_VERSION\[\] = \"'${version}'\";@' $files
+files=$(grep -rl --exclude=simple-sed.sh "const char LIBCELLML_VERSION\[\] = \"${version_regex}\";" *)
+sed -i "s@const char LIBCELLML_VERSION\[\] = \"${version_regex}\";@const char LIBCELLML_VERSION\[\] = \"${version}\";@" $files
 
-files=`grep -rl --exclude=simple-sed.sh "    expect(libcellml\.versionString()).toBe('\d*\.\d*\.\d*');" *`
-sed -i "s@    expect(libcellml\.versionString()).toBe('[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+');@    expect(libcellml\.versionString()).toBe('"${version}"');@" $files
+files=$(grep -rl --exclude=simple-sed.sh "    expect(libcellml\.versionString()).toBe('${version_regex}');" *)
+sed -i "s@    expect(libcellml\.versionString()).toBe('${version_regex}');@    expect(libcellml\.versionString()).toBe('${version}');@" $files
 
 git status
 echo "==================="
