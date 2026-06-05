eb4135634726f2145b28c64ff50cb7bb14d7906b
Alan Garny <agarny@hellix.com>
2026-06-05 19:55:26 +1200

    Update libCellML's version to 0.7.0.

--- tests/resources/coverage/generator/model.no.tracking.h
+++ tests/resources/coverage/generator/model.no.tracking.h
@@ -1,4 +1,4 @@
-/* The content of this file was generated using the C profile of libCellML 0.6.3. */
+/* The content of this file was generated using the C profile of libCellML 0.7.0. */
 
 #pragma once
 
--- tests/resources/generator/hodgkin_huxley_squid_axon_model_1952/model.variant.untracked.variables.py
+++ tests/resources/generator/hodgkin_huxley_squid_axon_model_1952/model.variant.untracked.variables.py
@@ -1,11 +1,11 @@
-# The content of this file was generated using the Python profile of libCellML 0.6.3.
+# The content of this file was generated using the Python profile of libCellML 0.7.0.
 
 from enum import Enum
 from math import *
 
 
 __version__ = "0.8.0"
-LIBCELLML_VERSION = "0.6.3"
+LIBCELLML_VERSION = "0.7.0"
 
 STATE_COUNT = 4
 CONSTANT_COUNT = 0
